94d97e0ecf4591684967c761157a1873140c347a
Guangyu Robert Yang <gyyang.neuro@gmail.com>
2019-02-22 15:51:15 -0500

    example register

---
+++ envs/test_new.py
@@ -0,0 +1,20 @@
+import gym
+from gym.envs.registration import register
+
+register(
+    id='Mante-v0',
+    entry_point='mante:Mante',
+    max_episode_steps=100000,
+    reward_threshold=90.0,
+)
+
+env = gym.make('Mante-v0')
+
+env.reset()
+for stp in range(11):
+    state, rew, status, info = env.step(env.action_space.sample())
+
+    print(state)
+    print(status)
+    print(rew)
+    print(info)
