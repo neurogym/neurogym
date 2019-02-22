72b36f8d7a8b8e150bc255106bef908755583cbd
manu <me@linux.com>
2019-02-22 10:27:26 -0500

    superclass describing tasks main structure

---
+++ envs/ngym.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 08:52:21 2019
+
+@author: molano
+"""
+
+import gym
+
+
+class ngym(gym.env):
+    """
+    two-alternative forced choice task where the probability of repeating the
+    previous choice is parametrized
+    """
+    def __init__(self, dt=0.1, n_act=3):
+        self.dt = dt
+        self.t = 0
+        self.trial = 1
+        # num actions
+        self.num_actions = n_act
+        # number of trials
+        self.num_tr = 0
+
+    def step(self, action):
+        """
+        receives an action and returns a new state, a reward, a flag variable
+        indicating whether the experiment has ended and a dictionary with
+        useful information
+        """
+        pass
+
+    def reset(self):
+        """
+        restarts the experiment with the same parameters
+        """
+        pass
+
+    def render(self, mode='human'):
+        """
+        plots relevant variables/parameters
+        """
+        pass
+
+    # Auxiliary functions
+    def _new_trial(self):
+        """
+        starts a new trials within the current experiment
+        """
+        pass
+
+    def analysis():
+        """
+        performs behavioral analysis relevant for the task
+        (i.e. psychometric cuves)
+        """
+        pass
