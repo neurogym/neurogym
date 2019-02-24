7ab9eea51d4498aa9c30e6bf83681d95814ffc3a
molano <mmolano0@hotmail.com>
2019-02-24 14:20:11 -0500

    reorganize code and PEP8

--- envs/rdm.py
+++ envs/rdm.py
@@ -18,135 +18,163 @@ Perceptual decision-making task, based on
 from __future__ import division
 
 import numpy as np
+from gym import spaces
+import tasktools
+import ngym
 
-from pyrl import tasktools
 
-# Time step
-dt = 1
-
-# Inputs
-inputs = tasktools.to_map('FIXATION', 'LEFT', 'RIGHT')
-
-# Actions
-actions = tasktools.to_map('FIXATE', 'CHOOSE-LEFT', 'CHOOSE-RIGHT')
-
-# Trial conditions
-left_rights  = [-1, 1]
-cohs         = [0, 6.4, 12.8, 25.6, 51.2]
-n_conditions = len(left_rights)*len(cohs)
-
-# Training
-n_gradient   = n_conditions
-n_validation = 100*n_conditions
-
-# Input noise
-sigma = np.sqrt(2*100*0.01)
-
-# Durations
-fixation      = 750
-stimulus_min  = 80
-stimulus_mean = 330
-stimulus_max  = 1500
-decision      = 500
-tmax          = fixation + stimulus_max + decision
-
-# Rewards
-R_ABORTED = -1
-R_CORRECT = +1
-
-def get_condition(rng, dt, context={}):
-    #-------------------------------------------------------------------------------------
-    # Epochs
-    #-------------------------------------------------------------------------------------
-
-    stimulus = context.get('stimulus')
-    if stimulus is None:
-        stimulus = tasktools.truncated_exponential(rng, dt, stimulus_mean,
-                                                   xmin=stimulus_min, xmax=stimulus_max)
-
-    durations = {
-        'fixation':  (0, fixation),
-        'stimulus':  (fixation, fixation + stimulus),
-        'decision':  (fixation + stimulus, fixation + stimulus + decision),
-        'tmax':      tmax
-        }
-    time, epochs = tasktools.get_epochs_idx(dt, durations)
-
-    #-------------------------------------------------------------------------------------
-    # Trial
-    #-------------------------------------------------------------------------------------
-
-    left_right = context.get('left_right')
-    if left_right is None:
-        left_right = rng.choice(left_rights)
-
-    coh = context.get('coh')
-    if coh is None:
-        coh = rng.choice(cohs)
-
-    return {
-        'durations':   durations,
-        'time':        time,
-        'epochs':      epochs,
-        'left_right':  left_right,
-        'coh':         coh
-        }
-
-# Input scaling
-def scale(coh):
-    return (1 + coh/100)/2
-
-def get_step(rng, dt, trial, t, a):
-    #-------------------------------------------------------------------------------------
-    # Reward
-    #-------------------------------------------------------------------------------------
-
-    epochs = trial['epochs']
-    status = {'continue': True}
-    reward = 0
-    if t-1 not in epochs['decision']:
-        if a != actions['FIXATE']:
-            status['continue'] = False
-            reward = R_ABORTED
-    elif t-1 in epochs['decision']:
-        if a == actions['CHOOSE-LEFT']:
-            status['continue'] = False
-            status['choice']   = 'L'
-            status['t_choice'] = t-1
-            status['correct']  = (trial['left_right'] < 0)
-            if status['correct']:
-                reward = R_CORRECT
-        elif a == actions['CHOOSE-RIGHT']:
-            status['continue'] = False
-            status['choice']   = 'R'
-            status['t_choice'] = t-1
-            status['correct']  = (trial['left_right'] > 0)
-            if status['correct']:
-                reward = R_CORRECT
-
-    #-------------------------------------------------------------------------------------
+class RDM(ngym.ngym):
     # Inputs
-    #-------------------------------------------------------------------------------------
+    inputs = tasktools.to_map('FIXATION', 'LEFT', 'RIGHT')
 
-    if trial['left_right'] < 0:
-        high = inputs['LEFT']
-        low  = inputs['RIGHT']
-    else:
-        high = inputs['RIGHT']
-        low  = inputs['LEFT']
+    # Actions
+    actions = tasktools.to_map('FIXATE', 'CHOOSE-LEFT', 'CHOOSE-RIGHT')
 
-    u = np.zeros(len(inputs))
-    if t in epochs['fixation'] or t in epochs['stimulus']:
-        u[inputs['FIXATION']] = 1
-    if t in epochs['stimulus']:
-        u[high] = scale(+trial['coh']) + rng.normal(scale=sigma)/np.sqrt(dt)
-        u[low]  = scale(-trial['coh']) + rng.normal(scale=sigma)/np.sqrt(dt)
+    # Trial conditions
+    left_rights = [-1, 1]
+    cohs = [0, 6.4, 12.8, 25.6, 51.2]
+    n_conditions = len(left_rights)*len(cohs)
 
-    #-------------------------------------------------------------------------------------
+    # Training
+    n_gradient = n_conditions
+    n_validation = 100*n_conditions
 
-    return u, reward, status
+    # Input noise
+    sigma = np.sqrt(2*100*0.01)
 
-def terminate(perf):
-    p_decision, p_correct = tasktools.correct_2AFC(perf)
+    # Durations
+    fixation = 750
+    stimulus_min = 80
+    stimulus_mean = 330
+    stimulus_max = 1500
+    decision = 500
+    tmax = fixation + stimulus_max + decision
 
-    return p_decision >= 0.99 and p_correct >= 0.8
+    # Rewards
+    R_ABORTED = -1
+    R_CORRECT = +1
+
+    def __init__(self, dt=100):
+        super().__init__(dt=dt)
+        high = np.array([1])
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+
+        self.seed()
+        self.viewer = None
+
+        self.steps_beyond_done = None
+
+        self.trial = self.get_condition(self.rng, self.dt)
+
+    def get_condition(self, rng, dt, context={}):
+        # ---------------------------------------------------------------------
+        # Epochs
+        # ---------------------------------------------------------------------
+
+        stimulus = context.get('stimulus')
+        if stimulus is None:
+            stimulus = tasktools.truncated_exponential(rng, dt,
+                                                       self.stimulus_mean,
+                                                       xmin=self.stimulus_min,
+                                                       xmax=self.stimulus_max)
+
+        durations = {
+            'fixation':  (0, self.fixation),
+            'stimulus':  (self.fixation, self.fixation + stimulus),
+            'decision':  (self.fixation + stimulus,
+                          self.fixation + stimulus + self.decision),
+            'tmax':      self.tmax
+            }
+        time, epochs = tasktools.get_epochs_idx(dt, durations)
+
+        # ---------------------------------------------------------------------
+        # Trial
+        # ---------------------------------------------------------------------
+
+        left_right = context.get('left_right')
+        if left_right is None:
+            left_right = rng.choice(self.left_rights)
+
+        coh = context.get('coh')
+        if coh is None:
+            coh = rng.choice(self.cohs)
+
+        return {
+            'durations':   durations,
+            'time':        time,
+            'epochs':      epochs,
+            'left_right':  left_right,
+            'coh':         coh
+            }
+
+    # Input scaling
+    def scale(self, coh):
+        return (1 + coh/100)/2
+
+    def step(self, action):
+        # ---------------------------------------------------------------------
+        # Reward
+        # ---------------------------------------------------------------------
+        trial = self.trial
+        epochs = trial['epochs']
+        status = {'continue': True}
+        reward = 0
+        if self.t-1 not in epochs['decision']:
+            if action != self.actions['FIXATE']:
+                status['continue'] = False
+                reward = self.R_ABORTED
+        elif self.t-1 in epochs['decision']:
+            if action == self.actions['CHOOSE-LEFT']:
+                status['continue'] = False
+                status['choice'] = 'L'
+                status['t_choice'] = self.t-1
+                status['correct'] = (trial['left_right'] < 0)
+                if status['correct']:
+                    reward = self.R_CORRECT
+            elif action == self.actions['CHOOSE-RIGHT']:
+                status['continue'] = False
+                status['choice'] = 'R'
+                status['t_choice'] = self.t-1
+                status['correct'] = (trial['left_right'] > 0)
+                if status['correct']:
+                    reward = self.R_CORRECT
+
+        # ---------------------------------------------------------------------
+        # Inputs
+        # ---------------------------------------------------------------------
+
+        if trial['left_right'] < 0:
+            high = self.inputs['LEFT']
+            low = self.inputs['RIGHT']
+        else:
+            high = self.inputs['RIGHT']
+            low = self.inputs['LEFT']
+
+        obs = np.zeros(len(self.inputs))
+        if self.t in epochs['fixation'] or self.t in epochs['stimulus']:
+            obs[self.inputs['FIXATION']] = 1
+        if self.t in epochs['stimulus']:
+            obs[high] = self.scale(+trial['coh']) +\
+                self.rng.normal(scale=self.sigma)/np.sqrt(self.dt)
+            obs[low] = self.scale(-trial['coh']) +\
+                self.rng.normal(scale=self.sigma)/np.sqrt(self.dt)
+
+        # ---------------------------------------------------------------------
+
+        # new trial?
+        if self.t > self.tmax/self.dt:
+            self.reset()
+        else:
+            self.t += 1
+        done = False  # TODO
+        return obs, reward, done, status
+
+    def reset(self):
+        self.trial = self.get_condition(self.rng, self.dt)
+        self.t = 0
+
+    def terminate(perf):
+        p_decision, p_correct = tasktools.correct_2AFC(perf)
+
+        return p_decision >= 0.99 and p_correct >= 0.8
