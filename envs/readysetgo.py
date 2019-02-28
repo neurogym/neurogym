f335539024500b753316e68eafcb95541f5ebc72
Guangyu Robert Yang <gyyang.neuro@gmail.com>
2019-02-28 15:37:19 -0500

    adding readysetgo task v0

---
+++ envs/readysetgo.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 13:48:19 2019
+
+@author: molano
+
+
+Ready-Set-Go task and Contextual Ready-Set-Go task, based on
+
+  Flexible Sensorimotor Computations through Rapid
+  Reconfiguration of Cortical Dynamics
+  Evan D. Remington, Devika Narain,
+  Eghbal A. Hosseini, Mehrdad Jazayeri, Neuron 2018.
+
+  https://www.cell.com/neuron/pdf/S0896-6273(18)30418-5.pdf
+
+"""
+from __future__ import division
+
+import numpy as np
+from gym import spaces
+import tasktools
+import ngym
+
+
+class ReadySetGo(ngym.ngym):
+    # Inputs
+    inputs = tasktools.to_map('FIXATION', 'READY', 'SET')
+
+    # Actions
+    actions = tasktools.to_map('FIXATE', 'GO')
+
+    # Input noise
+    sigma = np.sqrt(2*100*0.01)
+
+    # Durations
+    fixation = 750
+    stimulus_min = 80
+    stimulus_mean = 330
+    stimulus_max = 1500
+    decision = 500
+    tmax = fixation + stimulus_max + decision
+
+    # Rewards
+    R_ABORTED = -1.
+    R_CORRECT = +1.
+    R_FAIL = 0.
+    R_MISS = 0.
+
+    def __init__(self, dt=100):
+        super().__init__(dt=dt)
+        self.stimulus_min = np.max([self.stimulus_min, dt])
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(3, ),
+                                            dtype=np.float32)
+
+        self.seed()
+        self.viewer = None
+
+        self.steps_beyond_done = None
+
+        self.trial = self._new_trial(self.rng, self.dt)
+        print('------------------------')
+        print('RDM task')
+        print('time step: ' + str(self.dt))
+        print('------------------------')
+
+    def _new_trial(self, rng, dt):
+        # TODO: why are rng and dt inputs?
+        # ---------------------------------------------------------------------
+        # Epochs
+        # ---------------------------------------------------------------------
+
+        fixation = 500
+        measure = rng.choice([500, 580, 660, 760, 840, 920, 1000])
+        self.measure = measure
+        gain = 1
+        production = measure * gain
+        self.production = production
+        ready = set = 83  # duration of ready and set cue
+        tmax = self.fixation + measure + set + 2*production
+
+        durations = {
+            'fixation':  (0, self.fixation),
+            'ready': (self.fixation, self.fixation + ready),
+            'measure': (self.fixation, self.fixation + measure),
+            'set': (self.fixation + measure,
+                    self.fixation + measure + set),
+            'production': (self.fixation + measure + set,
+                           self.fixation + measure + set + 2*production),
+            'tmax': tmax
+            }
+        time, epochs = tasktools.get_epochs_idx(dt, durations)
+
+        # ---------------------------------------------------------------------
+        # Trial
+        # ---------------------------------------------------------------------
+
+        return {
+            'durations':   durations,
+            'time':        time,
+            'epochs':      epochs,
+            }
+
+    def step(self, action):
+        # ---------------------------------------------------------------------
+        # Reward
+        # ---------------------------------------------------------------------
+        trial = self.trial
+        status = {'continue': True}
+        reward = 0
+        tr_perf = False
+        # TODO: why do we use t-1?
+        # TODO: What's status continue? is it necessary?
+        # TODO: why not call status info, like gym?
+        # TODO: why use integer t instead of actual t?
+        # TODO: do we have intertrial interval in the beginning? can the network fixate at time 0?
+        if not self.in_epoch(self.t-1, 'production'):
+            if action != self.actions['FIXATE']:
+                status['continue'] = False
+                reward = self.R_ABORTED
+        else:
+            if action == self.actions['GO']:
+                status['continue'] = False  # terminate
+                # actual production time
+                t_prod = self.t*self.dt - trial['durations']['measure'][1]
+                eps = abs(t_prod - self.production)
+                eps_threshold = 0.2*self.production+25
+                if eps > eps_threshold:
+                    status['correct'] = False
+                    reward = self.R_FAIL
+                else:
+                    status['correct'] = True
+                    reward = (1. - eps/eps_threshold)**1.5
+                    reward = min(reward, 0.1)
+                    reward *= self.R_CORRECT
+                tr_perf = True
+
+                status['t_choice'] = self.t-1
+
+        # ---------------------------------------------------------------------
+        # Inputs
+        # ---------------------------------------------------------------------
+
+        obs = np.zeros(len(self.inputs))
+        obs[self.inputs['FIXATION']] = 1  # TODO: this is always on now
+        if self.in_epoch(self.t, 'ready'):
+            obs[self.inputs['READY']] = 1
+        if self.in_epoch(self.t, 'set'):
+            obs[self.inputs['SET']] = 1
+
+        # ---------------------------------------------------------------------
+        # new trial?
+        # TODO: it's confusing that the 4-th input is continue, but the function wants "status"
+        reward, new_trial = tasktools.new_trial(self.t, self.tmax, self.dt,
+                                                status['continue'],
+                                                self.R_MISS, reward)
+
+        if new_trial:
+            self.t = 0
+            self.num_tr += 1
+            # compute perf
+            self.perf, self.num_tr, self.num_tr_perf =\
+                tasktools.compute_perf(self.perf, reward, self.num_tr,
+                                       self.p_stp, self.num_tr_perf, tr_perf)
+            self.trial = self._new_trial(self.rng, self.dt)
+        else:
+            self.t += 1
+
+        done = False  # TODO: revisit
+        return obs, reward, done, status
+
+    def terminate(perf):
+        p_decision, p_correct = tasktools.correct_2AFC(perf)
+
+        return p_decision >= 0.99 and p_correct >= 0.8
