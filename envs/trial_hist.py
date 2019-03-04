a4bea78be83220ffedf7d22e01e338b0df99c3c3
molano <mmolano0@hotmail.com>
2019-03-04 12:49:57 -0600

    call _step function to be able to modify _new_trial

--- envs/trial_hist.py
+++ envs/trial_hist.py
@@ -9,6 +9,7 @@ Created on Fri Mar  1 11:48:59 2019
 from gym.core import Wrapper
 import tasktools
 
+
 class TrialHistory(Wrapper):
     """
     modfies a given environment by changing the probability of repeating the
@@ -19,7 +20,7 @@ class TrialHistory(Wrapper):
         self.env = env
         self.rep_prob = rep_prob
         # keeps track of the repeating prob of the current block
-        self.curr_block = tasktools.choice(self.rng, [0, 1])
+        self.curr_block = tasktools.choice(self.env.rng, [0, 1])
         # duration of block (in number oif trials)
         self.block_dur = block_dur
 
@@ -28,6 +29,10 @@ class TrialHistory(Wrapper):
         # Epochs
         # ---------------------------------------------------------------------
         trial = self.env._new_trial()
+        # change rep. prob. every self.block_dur trials
+        if self.env.num_tr % self.block_dur == 0:
+            self.curr_block = int(not self.curr_block)
+
         if self.prev_trial == -1:
             probs = (self.rep_prob[self.curr_block],
                      1-self.rep_prob[self.curr_block])
@@ -35,9 +40,21 @@ class TrialHistory(Wrapper):
             probs = (1-self.rep_prob[self.curr_block],
                      self.rep_prob[self.curr_block])
 
-        left_right = self.rng.choice(self.left_rights,
-                                     p=probs)
-
+        trial['ground_truth'] = self.env.rng.choice(self.left_rights,
+                                                    p=probs)
         self.prev_trial = trial['ground_truth']
 
         return trial
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        obs, reward, done, info, new_trial = self.env._step(action)
+
+        if new_trial:
+            self.env.trial = self._new_trial()
+
+        return obs, reward, done, info
+
+        
