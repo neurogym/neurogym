fb4a3295313ce8b83c349d548a49c2fc6b2e2835
molano <mmolano0@hotmail.com>
2019-03-04 13:42:46 -0600

    add message to trial_hist

--- envs/trial_hist.py
+++ envs/trial_hist.py
@@ -24,6 +24,7 @@ class TrialHistory(Wrapper):
         # duration of block (in number oif trials)
         self.block_dur = block_dur
         self.prev_trial = self.env.trial['ground_truth']
+        print('trial history wrapper')
 
     def _new_trial(self):
         # ---------------------------------------------------------------------
