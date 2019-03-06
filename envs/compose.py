f80d439cb3652318896097995d5d603c6367a1a2
molano <mmolano0@hotmail.com>
2019-03-06 14:13:01 -0600

    compose wrapper

--- envs/compose.py
+++ envs/compose.py
@@ -6,53 +6,76 @@ Created on Mon Mar  4 17:49:35 2019
 @author: molano
 """
 
-from gym.core import Wrapper
 import tasktools
+from gym import spaces
+import numpy as np
+import itertools
 
 
-class compose(Wrapper):
+class compose():
     """
     combines two different tasks
     """
-    def __init__(self, env1, env2):
-        Wrapper.__init__(self, env=env)
-        self.env = env
-        self.rep_prob = rep_prob
-        # keeps track of the repeating prob of the current block
-        self.curr_block = tasktools.choice(self.env.rng, [0, 1])
-        # duration of block (in number oif trials)
-        self.block_dur = block_dur
-        self.prev_trial = self.env.trial['ground_truth']
+    def __init__(self, env1, env2, delay):
+        self.t = 0
+        self.delay = delay
+        self.delay_on = True
+        self.env1 = env1
+        self.env2 = env2
+        self.num_act1 = self.env1.action_space.n
+        self.num_act2 = self.env2.action_space.n
+        self.action_space = spaces.Discrete(self.num_act1 *
+                                            self.num_act2)
+        self.observation_space = \
+            spaces.Box(-np.inf, np.inf,
+                       shape=(self.env1.observation_space.shape[0] +
+                              self.env2.observation_space.shape[0]),
+                       dtype=np.float32)
+        self.action_split = list(itertools.product(np.arange(self.num_act1),
+                                                   np.arange(self.num_act2)))
+        # start trials
+        self.env1.trial = self.env1._new_trial()
+        self.env2.trial = self.env2._new_trial()
 
     def _new_trial(self):
-        # ---------------------------------------------------------------------
-        # Epochs
-        # ---------------------------------------------------------------------
-        trial = self.env._new_trial()
-        # change rep. prob. every self.block_dur trials
-        if self.env.num_tr % self.block_dur == 0:
-            self.curr_block = int(not self.curr_block)
-
-        if self.prev_trial == -1:
-            probs = (self.rep_prob[self.curr_block],
-                     1-self.rep_prob[self.curr_block])
-        else:
-            probs = (1-self.rep_prob[self.curr_block],
-                     self.rep_prob[self.curr_block])
-
-        trial['ground_truth'] = self.env.rng.choice(self.env.choices,
-                                                    p=probs)
-        self.prev_trial = trial['ground_truth']
-
-        return trial
+        self.env1.trial = self.env1._new_trial()
+        self.env2.trial = self.env2._new_trial()
 
     def reset(self):
         return self.env.reset()
 
     def step(self, action):
-        obs, reward, done, info, new_trial = self.env._step(action)
+        action1, action2 = self.action_split[action]
+        if self.env1_on:
+            obs1, reward1, done1, info1, new_trial1 = self.env1._step(action1)
+            self.env1_on = not new_trial1
+        else:
+            obs1, reward1, done1 = self.standby_step(1)
 
-        if new_trial:
+        if self.t > self.delay and self.env2_on:
+            obs2, reward2, done2, info2, new_trial2 = self.env2._step(action2)
+            self.env2_on = not new_trial2
+        else:
+            obs2, reward2, done2 = self.standby_step(2)
+
+        if not self.env1_on and not self.env2_on:
             self.env.trial = self._new_trial()
+            self.t = 0
 
+        self.t += 1
+
+        obs = np.concatenate((obs1, obs2), axis=0)
+        reward = reward1 + reward2
+        done = done1  # TODO
+        info = {}  # TODO
         return obs, reward, done, info
+
+    def standby_step(self, env):
+        if env == 1:
+            obs = np.zeros((self.env1.observation_space.shape[0], ))
+        else:
+            obs = np.zeros((self.env2.observation_space.shape[0], ))
+        rew = 0
+        done = False
+        return obs, rew, done
+
