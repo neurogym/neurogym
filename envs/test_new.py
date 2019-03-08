1e04cae7804742c76df44d6a7d8c160d529edd04
molano <mmolano0@hotmail.com>
2019-03-07 18:02:09 -0600

    add comments organize code

--- envs/test_new.py
+++ envs/test_new.py
@@ -8,26 +8,36 @@ import reaction_time
 import compose
 import manage_data as md
 import matplotlib.pyplot as plt
-# params = {'trial_dur': 5000, 'dt': 500,
-#           'stim_ev': 0.1, 'rewards':(0.0, -.1, 1., -1.)}
-
-# example code        task   num. steps  wrapper
+dt = 100
+# calling from the terminal
+# example            task   num. steps  wrapper
 # python test_new.py RDM-v0    1000    trial_hist
-env = gym.make(sys.argv[1])
-if sys.argv[3] == 'trial_hist':
+if len(sys.argv) > 0:
+    params = {'task': sys.argv[1], 'num_steps': sys.argv[2],
+              'wrapper': sys.argv[3]}
+else:
+    params = {'task': 'GNG-v0', 'num_steps': 1000, 'wrapper': ''}
+
+# task
+env = gym.make(params['task'], **{'dt': dt})
+# wrappers
+if params['wrapper'] == 'trial_hist':
     env = trial_hist.TrialHistory(env)
-elif sys.argv[3] == 'reaction_time':
+elif params['wrapper'] == 'reaction_time':
     env = reaction_time.ReactionTime(env)
-elif sys.argv[3] == 'compose':
-    env_extra = gym.make('GNG-v0')
-    env = compose.compose(env, env_extra)
+elif params['wrapper'] == 'compose':
+    env_extra = gym.make('GNG-v0', **{'dt': dt})
+    # delay is in ms
+    env = compose.compose(dt=dt, env1=env, env2=env_extra, delay=200)
+
+# save/render data wrapper
 env = md.manage_data(env, plt_tr=False)
 env.reset()
 observations = []
 rewards = []
 actions = []
 actions_end_of_trial = []
-for stp in range(int(sys.argv[2])):
+for stp in range(int(params['num_steps'])):
     action = env.action_space.sample()
     state, rew, done, info = env.step(action)
     if done:
@@ -37,7 +47,7 @@ for stp in range(int(sys.argv[2])):
     if 'new_trial' in info.keys():
         actions_end_of_trial.append(action)
     else:
-        actions_end_of_trial.append(0)
+        actions_end_of_trial.append(-1)
     rewards.append(rew)
     actions.append(action)
 
@@ -47,7 +57,7 @@ plt.subplot(3, 1, 1)
 plt.imshow(obs.T, aspect='auto')
 plt.title('observations')
 plt.subplot(3, 1, 2)
-plt.plot(actions)
+plt.plot(actions, marker='+')
 plt.plot(actions_end_of_trial, '--')
 plt.title('actions')
 plt.xlim([0, len(rewards)])
