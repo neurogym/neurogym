abd3eac11a686a2f92383be2b82435e278046a6c
molano <mmolano0@hotmail.com>
2019-03-11 17:34:49 -0500

    reduce fixation period

--- envs/rdm.py
+++ envs/rdm.py
@@ -41,7 +41,7 @@ class RDM(ngym.ngym):
         self.sigma = np.sqrt(2*100*0.01)
 
         # Durations
-        self.fixation = 750
+        self.fixation = 100
         self.stimulus_min = 499  # 80
         self.stimulus_mean = 500  # 330
         self.stimulus_max = 501  # 1500
