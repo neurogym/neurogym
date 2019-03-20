977277d5f0f94bd2643301379bf0927fd72d7901
molano <mmolano0@hotmail.com>
2019-03-20 15:21:37 -0500

    add pass-reward option to call function

--- envs/call_function.py
+++ envs/call_function.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     alg = 'a2c'
     env = 'RDM-v0'
     net = 'cont_rnn'
-    nsteps = 50
-    tot_num_stps = 1e8
+    nsteps = 100
+    tot_num_stps = 1e10
     li = 1e2
     ent_coef = 0.1
     lr = 1e-3
@@ -30,11 +30,18 @@ if __name__ == '__main__':
     gamma = 0.9
     num_env = 12
     trial_hist = True
+    pass_reward = True
     if trial_hist:
         tr_h_flag = 'trHist'
     else:
         tr_h_flag = ''
-    save_path = '../' + alg + '_' + env + '_' + tr_h_flag +\
+
+    if pass_reward:
+        pss_rw_flag = 'pssRew'
+    else:
+        pss_rw_flag = ''
+
+    save_path = '../' + alg + '_' + env + '_' + tr_h_flag + pss_rw_flag +\
         '_' + net + '_ent_coef_' + str(ent_coef) + '_lr_' + str(lr) +\
         '_lrsch_' + lr_sch + '_g_' + str(gamma) +\
         '_batch_' + num2str(nsteps) + '_dur_' + num2str(tot_num_stps) +\
@@ -51,7 +58,8 @@ if __name__ == '__main__':
         ' --log_interval=' + str(li) + ' --ent_coef=' + str(ent_coef) +\
         ' --lrschedule=' + lr_sch + ' --gamma=' + str(gamma) +\
         ' --num_env=' + str(num_env) + ' --trial_hist=' + str(trial_hist) +\
-        ' --lr=' + str(lr) + ' --save_path=' + save_path
+        ' --pass_reward=' + str(pass_reward) + ' --lr=' + str(lr) +\
+        ' --save_path=' + save_path
     # + ' --load_path=' + load_path
     print(command)
     os.system(command)
