dee55d687c0fc6eacc927963aeb4ed7197e052c8
molano <you@example.com>
2019-04-11 09:24:55 -0500

    create sh files, remove sys.path.append cmmds

--- call_function.py
+++ call_function.py
@@ -6,19 +6,14 @@ Created on Thu Mar 14 08:20:00 2019
 @author: linux
 """
 import os
-import sys
 import numpy as np
 import itertools
 import matplotlib
 from pathlib import Path
 from datetime import datetime
+from neurogym.ops import utils as ut
 home = str(Path.home())
 matplotlib.use('Agg')
-sys.path.append('/rigel/home/mm5514/neurogym')
-from neurogym.ops import utils as ut
-sys.path.append('/rigel/home/mm5514/gym')
-sys.path.append('/rigel/home/mm5514/n_baselines')
-sys.path.append('/rigel/home/mm5514/joblib-master')
 
 
 def build_command(ps_r=True, ps_act=True, bl_dur=200, num_u=32, stimEv=1.,
@@ -100,7 +95,7 @@ def build_command(ps_r=True, ps_act=True, bl_dur=200, num_u=32, stimEv=1.,
     # load_path = save_path + '/checkpoints/00020'
     if not os.path.exists(save_path) and save:
         os.mkdir(save_path)
-    command = home + '/run.py --alg=' + alg
+    command = '/rigel/home/mm5514/' + 'run.py --alg=' + alg
     command += ' --env=' + env
     command += ' --network=' + net_type
     command += ' --nsteps=' + str(nsteps)
@@ -120,7 +115,7 @@ def build_command(ps_r=True, ps_act=True, bl_dur=200, num_u=32, stimEv=1.,
     command += ps_a_cmmd
     command += timing_cmmd
     command += load_path_cmmd
-    command += ' --figs=True'
+    # command += ' --figs=True'
     print('Path: ')
     print(save_path)
     if save:
@@ -134,6 +129,60 @@ def build_command(ps_r=True, ps_act=True, bl_dur=200, num_u=32, stimEv=1.,
         np.savez(save_path + '/params.npz', ** params)
 
     return command, save_path
+
+
+def produce_sh_files():
+    home = str(Path.home())
+    pass_reward = True
+    pass_action = True
+    bl_dur = [200]
+    num_units = [32, 64, 128]
+    net_type = ['twin_net', 'cont_rnn']
+    num_steps_env = 1e8  # [1e9]
+    stim_ev = [.3, .6, 1.]
+    batch_size = [5, 20, 50]
+    insts = np.arange(5)
+    load_path = ''  # '/home/linux/00010'
+    params_config = itertools.product(insts, bl_dur, num_units, stim_ev,
+                                      batch_size, net_type)
+    main_file = file = open(home + '/scripts/main.sh', 'w')
+    command = ''
+    command += '#!/bin/sh\n'
+    command += '#SBATCH --account=theory\n'
+    command += '#SBATCH --job-name=RUN\n'
+    command += '#SBATCH -c 1\n'
+    command += '#SBATCH --time=0:10:00\n'
+    command += '#SBATCH --mem-per-cpu=128gb\n'
+    command += '#SBATCH --exclusive\n'
+    main_file.write(command)
+    command = ''
+    command += '#!/bin/sh\n'
+    command += '#SBATCH --account=theory\n'
+    command += '#SBATCH --job-name=RUN\n'
+    command += '#SBATCH -c 1\n'
+    command += '#SBATCH --time=120:00:00\n'
+    command += '#SBATCH --mem-per-cpu=128gb\n'
+    command += '#SBATCH --exclusive\n'
+    command += 'module load anaconda/3-5.1\n'
+    command += 'module load tensorflow/anaconda3-5.1.0/1.7.0\n'
+    for conf in params_config:
+        name = 'scripts/' + str(conf[1]) + '_' + str(conf[2]) +\
+            '_' + str(conf[3]) + '_' + str(conf[4]) + '_' +\
+            str(conf[5]) + '_' + str(conf[0]) + '.sh'
+        main_file.write('sbatch ' + name + '\n')
+        main_file.write('sleep 10\n')
+        file = open(home + '/' + name, 'w')
+        cmmd = command
+        aux, _ = build_command(inst=conf[0], ps_r=pass_reward,
+                               ps_act=pass_action,
+                               bl_dur=conf[1], num_u=conf[2],
+                               net_type=conf[5], num_stps_env=num_steps_env,
+                               load_path=load_path, stimEv=conf[3],
+                               nsteps=conf[4], save=False)
+        cmmd += aux
+        file.write(cmmd)
+        file.close()
+    main_file.close()
 
 
 def arg_parser():
@@ -152,27 +201,26 @@ def neuro_arg_parser():
     parser = arg_parser()
     parser.add_argument('--num_insts',
                         help='number of instances to run',
-                        type=bool, default=True)
+                        type=int, default=1)
     parser.add_argument('--pass_reward',
                         help='whether to pass the prev. reward with obs',
                         type=bool, default=True)
     parser.add_argument('--pass_action',
                         help='whether to pass the prev. action with obs',
                         type=bool, default=True)
-    parser.add_argument('--net_type',
-                        help='type of architecture',
+    parser.add_argument('--net_type', help='type of architecture',
                         type=str, default='twin_net')
     parser.add_argument('--num_u',
                         help='number of total units',
-                        type=int, nargs='+', default=(32))
+                        type=int, nargs='+', default=(32,))
     parser.add_argument('--n_steps',
                         help='rollout',
-                        type=float, nargs='+', default=(20))
+                        type=float, nargs='+', default=(20,))
     parser.add_argument('--bl_dur',
                         help='dur. of block in the trial-hist wrappr (trials)',
-                        type=int, nargs='+', default=(200))
+                        type=int, nargs='+', default=(200,))
     parser.add_argument('--stimEv', help='allows scaling stimulus evidence',
-                        type=float, nargs='+', default=(1.))
+                        type=float, nargs='+', default=(1.,))
     return parser
 
 
@@ -184,25 +232,27 @@ def main(args):
     bl_dur = args.bl_dur
     num_units = args.num_u
     net_type = args.net_type
-    num_steps_env = [1e8]  # [1e9]
+    num_steps_env = 1e8  # [1e9]
     stim_ev = args.stimEv
     batch_size = args.n_steps
     insts = np.arange(args.num_insts)
     load_path = ''  # '/home/linux/00010'
-    params_config = itertools.product(insts, pass_reward, pass_action, bl_dur,
-                                      num_units, net_type, num_steps_env,
-                                      stim_ev, batch_size)
+    params_config = itertools.product(insts, bl_dur, num_units, stim_ev,
+                                      batch_size)
     batch_command = ''
     for conf in params_config:
         print('---------------------------------------------')
-        cmmd, _ = build_command(inst=conf[0], ps_r=conf[1], ps_act=conf[2],
-                                bl_dur=conf[3], num_u=conf[4],
-                                net_type=conf[5], num_stps_env=conf[6],
-                                load_path=load_path, stimEv=conf[7],
-                                nsteps=conf[8])
+        cmmd, _ = build_command(inst=conf[0], ps_r=pass_reward,
+                                ps_act=pass_action,
+                                bl_dur=conf[1], num_u=conf[2],
+                                net_type=net_type, num_stps_env=num_steps_env,
+                                load_path=load_path, stimEv=conf[3],
+                                nsteps=conf[4])
         batch_command += cmmd + '\n'
     os.system(batch_command)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    produce_sh_files()
+    #    asdsad
+    #    main(sys.argv)
