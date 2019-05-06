b5a539c981996ad2d0fe76973db929d16cd29ffb
molano <manuelmolanomazon@gmail.com>
2019-05-06 12:23:37 -0500

    add twin_net exp

--- call_function.py
+++ call_function.py
@@ -353,6 +353,7 @@ if __name__ == '__main__':
                      num_insts=num_insts, experiment=experiment,
                      main_folder=main_folder, num_steps_env=num_steps_env)
     command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
     # NUMBER OF NEURONS EXPERIMENT
     hours = '4'
     alg = ['supervised']
@@ -375,6 +376,7 @@ if __name__ == '__main__':
                      num_insts=num_insts, experiment=experiment,
                      main_folder=main_folder, num_steps_env=num_steps_env)
     command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
     # ROLLOUT
     hours = '4'
     alg = ['supervised']
@@ -397,6 +399,7 @@ if __name__ == '__main__':
                      num_insts=num_insts, experiment=experiment,
                      main_folder=main_folder, num_steps_env=num_steps_env)
     command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
     # A2C (RL). PASS REWARD/ACTION EXPERIMENT
     hours = '4'
     alg = ['a2c']
@@ -419,6 +422,7 @@ if __name__ == '__main__':
                      num_insts=num_insts, experiment=experiment,
                      main_folder=main_folder, num_steps_env=num_steps_env)
     command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
     # REPEATING PROBABILITY
     hours = '4'
     alg = ['supervised']
@@ -485,5 +489,29 @@ if __name__ == '__main__':
                      num_insts=num_insts, experiment=experiment,
                      main_folder=main_folder, num_steps_env=num_steps_env)
     command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
+    # TWIN NETWORK
+    hours = '4'
+    alg = ['supervised']
+    num_units = [32]
+    bl_dur = [200]
+    stim_ev = [.5]
+    batch_size = [20]
+    net_type = ['twin_net']
+    rep_prob = [[.2, .8]]
+    pass_r = [True]
+    pass_act = [True]
+    num_insts = 10
+    num_steps_env = 1e8
+    experiment = 'twin_net'
+    main_folder = '/rigel/theory/users/mm5514/'
+    produce_sh_files(cluster='hab', alg=alg, hours=hours, num_units=num_units,
+                     bl_dur=bl_dur, stim_ev=stim_ev, rep_prob=rep_prob,
+                     batch_size=batch_size, net_type=net_type,
+                     pass_r=pass_r, pass_act=pass_act,
+                     num_insts=num_insts, experiment=experiment,
+                     main_folder=main_folder, num_steps_env=num_steps_env)
+    command += 'sbatch ' + experiment + '/analysis_hab.sh\n'
+
     all_analysis_file.write(command)
     all_analysis_file.close()
