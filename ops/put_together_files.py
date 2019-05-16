fccd390faf476b2967adca576db487661fe5c611
Molano <molano@ibp10333.csc.es>
2019-05-16 12:14:08 +0200

    check loading files is ok

--- ops/put_together_files.py
+++ ops/put_together_files.py
@@ -26,36 +26,42 @@ def put_files_together(folder, min_num_trials=1e6):
     r_prob_mat = []
     side_mat = []
     for ind_f in range(len(files)):
-        data = np.load(files[ind_f])
-        choice = data['choice']
-        stim = data['stimulus']
-        r_prob = data['rep_prob']
-        side = data['correct_side']
-        side = side.reshape((side.shape[0], -1))
-        if side.shape[1] != 1:
-            side = np.argmax(side, axis=1)
-        else:
-            side = side.reshape((side.shape[0],))
-        if choice.shape[0] != side.shape[0]:
-            dec_time = np.where(stim[:, 0] == 0)[0]
-            dec_time_aux = np.concatenate((dec_time,
-                                           np.array([dec_time[-1]+2])))
-            dec_time_aux = np.diff(dec_time_aux)
-            assert (dec_time_aux >= 1).all()
-            dec_time = dec_time[dec_time_aux != 1]
-            choice = choice[dec_time]
-            stim = stim[dec_time-1, :]
-        choice_mat.append(choice)
-        stim_mat.append(stim)
-        r_prob_mat.append(r_prob)
-        side_mat.append(side)
-        if ind_f == 0:
-            SIZE = choice.shape
-        assert (SIZE == choice.shape), str(SIZE) + ' ' + str(choice.shape)
-        assert (SIZE[0] == stim.shape[0]), str(SIZE) + ' ' + str(stim.shape)
-        rps = r_prob.shape
-        assert (SIZE[0] == rps[0]), str(SIZE) + ' ' + str(rps)
-        assert (SIZE == side.shape), str(SIZE) + ' ' + str(side.shape)
+        loaded = True
+        try:
+            data = np.load(files[ind_f])
+        except:
+            loaded = False
+        if loaded:
+            choice = data['choice']
+            stim = data['stimulus']
+            r_prob = data['rep_prob']
+            side = data['correct_side']
+            side = side.reshape((side.shape[0], -1))
+            if side.shape[1] != 1:
+                side = np.argmax(side, axis=1)
+            else:
+                side = side.reshape((side.shape[0],))
+            if choice.shape[0] != side.shape[0]:
+                dec_time = np.where(stim[:, 0] == 0)[0]
+                dec_time_aux = np.concatenate((dec_time,
+                                               np.array([dec_time[-1]+2])))
+                dec_time_aux = np.diff(dec_time_aux)
+                assert (dec_time_aux >= 1).all()
+                dec_time = dec_time[dec_time_aux != 1]
+                choice = choice[dec_time]
+                stim = stim[dec_time-1, :]
+            choice_mat.append(choice)
+            stim_mat.append(stim)
+            r_prob_mat.append(r_prob)
+            side_mat.append(side)
+            if ind_f == 0:
+                SIZE = choice.shape
+            assert (SIZE == choice.shape), str(SIZE) + ' ' + str(choice.shape)
+            assert (SIZE[0] ==
+                    stim.shape[0]), str(SIZE) + ' ' + str(stim.shape)
+            rps = r_prob.shape
+            assert (SIZE[0] == rps[0]), str(SIZE) + ' ' + str(rps)
+            assert (SIZE == side.shape), str(SIZE) + ' ' + str(side.shape)
     if len(files) > 0:
         choice_mat = np.reshape(np.array(choice_mat), (SIZE[0]*len(files), ))
         stim_mat = np.reshape(np.array(stim_mat), (SIZE[0]*len(files),
