beac48f2cc04cc3e9d25ea34e5248625e9bbe0ed
Molano <molano@ibp10333.csc.es>
2019-05-27 12:01:04 +0200

    minor

--- ops/put_together_files.py
+++ ops/put_together_files.py
@@ -12,7 +12,6 @@ Created on Thu Apr  4 11:20:01 2019
 # 'act_mat',
 # 'rew_mat',
 # 'rep_prob']
-import os
 import glob
 import numpy as np
 from neurogym.ops import utils as ut
@@ -27,6 +26,7 @@ def put_files_together(folder, min_num_trials=1e6):
     r_prob_mat = []
     side_mat = []
     counter = 0
+    SIZE = 0
     for ind_f in range(len(files)):
         loaded = True
         try:
@@ -57,26 +57,26 @@ def put_files_together(folder, min_num_trials=1e6):
             stim_mat.append(stim)
             r_prob_mat.append(r_prob)
             side_mat.append(side)
-            if ind_f == 0:
-                SIZE = choice.shape
-            assert (SIZE == choice.shape), str(SIZE) + ' ' + str(choice.shape)
-            assert (SIZE[0] ==
-                    stim.shape[0]), str(SIZE) + ' ' + str(stim.shape)
-            rps = r_prob.shape
-            assert (SIZE[0] == rps[0]), str(SIZE) + ' ' + str(rps)
-            assert (SIZE == side.shape), str(SIZE) + ' ' + str(side.shape)
+            SIZE += choice.shape[0]
+#            if ind_f == 0:
+#                SIZE = choice.shape
+#            assert (SIZE == choice.shape), str(SIZE) + ' ' + str(choice.shape)
+#            assert (SIZE[0] ==
+#                    stim.shape[0]), str(SIZE) + ' ' + str(stim.shape)
+#            rps = r_prob.shape
+#            assert (SIZE[0] == rps[0]), str(SIZE) + ' ' + str(rps)
+#            assert (SIZE == side.shape), str(SIZE) + ' ' + str(side.shape)
     if counter > 0:
-        choice_mat = np.reshape(np.array(choice_mat), (SIZE[0]*counter, ))
-        stim_mat = np.reshape(np.array(stim_mat), (SIZE[0]*counter,
+        choice_mat = np.reshape(np.array(choice_mat), (SIZE, ))
+        stim_mat = np.reshape(np.array(stim_mat), (SIZE,
                                                    stim.shape[1]))
-        side_mat = np.reshape(np.array(side_mat), (SIZE[0]*counter, ))
+        side_mat = np.reshape(np.array(side_mat), (SIZE, ))
         # r_prob_mat = np.reshape(np.array(r_prob_mat), (SIZE[0]*counter,))
         r_prob_mat = []
         data = {'choice': choice_mat, 'stimulus': stim_mat,
                 'correct_side': side_mat, 'rep_prob': r_prob_mat}
         np.savez(folder + '/bhvr_data_all.npz', **data)
-        os.remove('*_bhvr_data*npz')
-        if SIZE[0]*counter > min_num_trials:
+        if SIZE*counter > min_num_trials:
             return True
         else:
             return False
