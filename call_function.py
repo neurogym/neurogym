1c3af6cf4433624c3b274e33c9b4ba44ebebdf81
Molano <molano@ibp10333.csc.es>
2019-06-21 08:53:33 +0200

    add parser to test_new

---
+++ call_function.py
@@ -0,0 +1,353 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 08:20:00 2019
+
+@author: linux
+"""
+import os
+import numpy as np
+import itertools
+import matplotlib
+from datetime import datetime
+from neurogym.ops import utils as ut
+from os.path import expanduser
+home = expanduser("~")
+matplotlib.use('Agg')
+
+
+def build_command(save_folder='/rigel/theory/users/mm5514/',
+                  run_folder='/rigel/home/mm5514/',
+                  ps_r=True, ps_act=True, bl_dur=200, num_u=32, stimEv=1.,
+                  net_type='twin_net', num_stps_env=1e9, load_path='',
+                  save=True, nsteps=20, alg='a2c', env='RDM-v0',
+                  seed=None, num_env=24, ent_coef=0.05, lr=1e-3,
+                  lr_sch='constant', gamma=.8, rep_prob=(.2, .8),
+                  timing=[100, 200, 200, 200, 100], save_folder_name='',
+                  eval_steps=100000, alpha=0.1):
+    if seed is None:
+        seed = datetime.now().microsecond
+    tot_num_stps = num_stps_env*num_env
+    num_steps_per_logging = 500000
+    li = num_steps_per_logging // nsteps
+    if net_type == 'twin_net':
+        nlstm = num_u // 2
+    else:
+        nlstm = num_u
+
+    if env == 'RDM-v0':
+        timing_flag = '_t_' + ut.list_str(timing)
+        timing_cmmd = ' --timing '
+        for ind_t in range(len(timing)):
+            timing_cmmd += str(timing[ind_t]) + ' '
+    else:
+        timing_flag = ''
+        timing_cmmd = ''
+    # trial history
+    trial_hist = True
+    if trial_hist:
+        tr_h_flag = '_TH_' + ut.list_str(rep_prob) + '_' + str(bl_dur)
+        tr_h_cmmd = ' --trial_hist=True --bl_dur=' + str(bl_dur) +\
+            ' --rep_prob '
+        for ind_rp in range(len(rep_prob)):
+            tr_h_cmmd += str(rep_prob[ind_rp]) + ' '
+    else:
+        tr_h_flag = ''
+        tr_h_cmmd = ''
+    # pass reward
+    if ps_r:
+        ps_rw_flag = '_PR'
+        ps_rw_cmmd = ' --pass_reward=True'
+    else:
+        ps_rw_flag = ''
+        ps_rw_cmmd = ''
+    # pass action
+    if ps_act:
+        ps_a_flag = '_PA'
+        ps_a_cmmd = ' --pass_action=True'
+    else:
+        ps_a_flag = ''
+        ps_a_cmmd = ''
+
+    load_path = load_path
+    if load_path == '':
+        load_path_cmmd = ''
+    else:
+        load_path_cmmd = ' --load_path=' + load_path
+
+    save_path = save_folder + alg + '_' + env +\
+        timing_flag + tr_h_flag + ps_rw_flag + ps_a_flag + '_' + net_type
+    save_path += '_ec_' + str(ent_coef)
+    save_path += '_lr_' + str(lr)
+    save_path += '_lrs_' + lr_sch
+    save_path += '_g_' + str(gamma)
+    save_path += '_b_' + ut.num2str(nsteps)
+    save_path += '_ne_' + str(num_env)
+    save_path += '_nu_' + str(nlstm)
+    save_path += '_ev_' + str(stimEv)
+    save_path += '_a_' + str(alpha)
+    save_path += '_' + str(seed)
+    save_path += save_folder_name
+    save_path = save_path.replace('-v0', '')
+    save_path = save_path.replace('constant', 'c')
+    save_path = save_path.replace('linear', 'l')
+    # load_path = save_path + '/checkpoints/00020'
+    if not os.path.exists(save_path) and save:
+        os.mkdir(save_path)
+    command = run_folder + 'run.py --alg=' + alg
+    command += ' --env=' + env
+    command += ' --network=' + net_type
+    command += ' --nsteps=' + str(nsteps)
+    command += ' --num_timesteps=' + str(tot_num_stps)
+    command += ' --log_interval=' + str(li)
+    command += ' --ent_coef=' + str(ent_coef)
+    command += ' --lrschedule=' + lr_sch
+    command += ' --gamma=' + str(gamma)
+    command += ' --num_env=' + str(num_env)
+    command += ' --lr=' + str(lr)
+    command += ' --save_path=' + save_path
+    command += ' --nlstm=' + str(nlstm)
+    command += ' --stimEv=' + str(stimEv)
+    command += ' --seed=' + str(seed)
+    command += ' --eval_steps=' + str(eval_steps)
+    command += ' --alpha=' + str(alpha)
+    command += tr_h_cmmd
+    command += ps_rw_cmmd
+    command += ps_a_cmmd
+    command += timing_cmmd
+    command += load_path_cmmd
+    # command += ' --figs=True'
+    if save:
+        print('Command:')
+        print(command)
+        vars_ = vars()
+        params = {x: vars_[x] for x in vars_.keys() if type(vars_[x]) == str or
+                  type(vars_[x]) == int or type(vars_[x]) == float or
+                  type(vars_[x]) == bool or type(vars_[x]) == list or
+                  type(vars_[x]) == tuple}
+        np.savez(save_path + '/params.npz', ** params)
+
+    return command, save_path
+
+
+def produce_sh_files(cluster='hab', alg=['a2c'], hours='120', num_units=[32],
+                     bl_dur=[10, 40, 500, 10000], stim_ev=[.5],
+                     rep_prob=[[.2, .8]], batch_size=[20],
+                     net_type=['cont_rnn'], pass_r=[True], pass_act=[True],
+                     num_insts=5, experiment='', main_folder='',
+                     num_steps_env=1e8, alpha=[0.1]):
+    if cluster == 'hab':
+        save_folder = main_folder + experiment + '/'
+        run_folder = '/rigel/home/mm5514/'
+        n_envs = 24
+    else:
+        save_folder = main_folder + experiment + '/'
+        run_folder = '/home/hcli64/hcli64348/'
+        n_envs = 40
+    insts = np.arange(num_insts)
+    params_config = itertools.product(batch_size, bl_dur, num_units, stim_ev,
+                                      net_type, pass_r, pass_act, alg,
+                                      rep_prob, alpha, insts)
+    scr_folder = scripts_folder + experiment + '/'
+    if not os.path.exists(scr_folder):
+        os.makedirs(scr_folder)
+    main_file = open(scr_folder + 'main_' +
+                     cluster + '.sh', 'w')
+    command = specs(cluster=cluster, hours='4')
+    main_file.write(command)
+
+    for conf in params_config:
+        print('-----------------------')
+        name = str(conf[1]) + '_' + str(conf[2]) +\
+            '_' + str(conf[3]) + '_' + str(conf[4]) + '_' +\
+            '_' + str(conf[5]) + '_' + str(conf[6]) + '_' +\
+            str(conf[7]) + '_' + str(conf[0]) + '_' + ut.list_str(conf[8]) +\
+            '_' + str(conf[9]) + '_' + str(conf[10]) + '_' + hours + 'h_' +\
+            cluster
+        name = name.replace('.', '')
+        name += '.sh'
+        print(name)
+        main_file.write('sbatch ' + name + '\n')
+        main_file.write('sleep 20\n')
+        # training script
+        file = open(scr_folder + name, 'w')
+        cmmd = specs(conf=conf, cluster=cluster, hours=hours, alg=conf[7],
+                     name=name, n_envs=n_envs)
+        aux, _ = build_command(save_folder=save_folder, run_folder=run_folder,
+                               ps_r=conf[5], ps_act=conf[6], rep_prob=conf[8],
+                               bl_dur=conf[1], num_u=conf[2], num_env=n_envs,
+                               net_type=conf[4], num_stps_env=num_steps_env,
+                               load_path='', stimEv=conf[3],
+                               nsteps=conf[0], save=False, alg=conf[7],
+                               eval_steps=0, alpha=conf[9])
+        cmmd += aux
+        file.write(cmmd)
+        file.close()
+
+    main_file.close()
+    analysis_file = open(scr_folder + 'analysis_' +
+                         cluster + '.sh', 'w')
+    command = specs(cluster=cluster, hours='2')
+    command += run_folder + 'analysis.py ' + save_folder
+    analysis_file.write(command)
+    analysis_file.close()
+
+
+def specs(conf=None, cluster='hab', hours='120', alg='a2c', name='',
+          n_envs=24):
+    command = ''
+    command += '#!/bin/sh\n'
+    if cluster == 'hab':
+        command += '#SBATCH --account=theory\n'
+        if conf is None:
+            command += '#SBATCH --job-name=RUN\n'
+            command += '#SBATCH -c 1\n'
+            command += '#SBATCH --time=' + hours + ':00:00\n'
+            command += '#SBATCH --mem-per-cpu=128gb\n'
+            command += 'module load anaconda/3-5.1\n'
+            command += 'module load tensorflow/anaconda3-5.1.0/1.7.0\n'
+        else:
+            name = name[:-3] + '_' + hours
+            command += '#SBATCH --job-name=' + name + '\n'
+            command += '#SBATCH --cpus-per-task=' + str(n_envs) + '\n'
+            command += '#SBATCH --time=' + hours + ':00:00\n'
+            command += '#SBATCH --mem-per-cpu=5gb\n'
+            command += '#SBATCH --exclusive\n'
+            command += 'module load anaconda/3-5.1\n'
+            command += 'module load tensorflow/anaconda3-5.1.0/1.7.0\n'
+    else:
+        if conf is None:
+            command += '#SBATCH --job-name=RUN\n'
+            command += '#SBATCH -c 1\n'
+            command += '#SBATCH --time=' + hours + ':00:00\n'
+            command += 'module purge\n'
+            command += 'module load gcc/6.4.0\n'
+            command += 'module load cuda/9.1\n'
+            command += 'module load cudnn/7.1.3\n'
+            command += 'module load openmpi/3.0.0\n'
+            command += 'module load atlas/3.10.3\n'
+            command += 'module load scalapack/2.0.2\n'
+            command += 'module load fftw/3.3.7\n'
+            command += 'module load szip/2.1.1\n'
+            command += 'module load opencv/3.4.1\n'
+            command += 'module load python/3.6.5_ML\n'
+        else:
+            name = name[:-3] + '_' + hours
+            command += '#SBATCH --job-name=' + name + '\n'
+            command += '#SBATCH --cpus-per-task=' + str(n_envs) + '\n'
+            command += '#SBATCH --time=' + hours + ':00:00\n'
+            command += '#SBATCH --exclusive\n'
+            command += 'module purge\n'
+            command += 'module load gcc/6.4.0\n'
+            command += 'module load cuda/9.1\n'
+            command += 'module load cudnn/7.1.3\n'
+            command += 'module load openmpi/3.0.0\n'
+            command += 'module load atlas/3.10.3\n'
+            command += 'module load scalapack/2.0.2\n'
+            command += 'module load fftw/3.3.7\n'
+            command += 'module load szip/2.1.1\n'
+            command += 'module load opencv/3.4.1\n'
+            command += 'module load python/3.6.5_ML\n'
+
+    return command
+
+
+def arg_parser():
+    """
+    Create an empty argparse.ArgumentParser.
+    """
+    import argparse
+    aadhf = argparse.ArgumentDefaultsHelpFormatter
+    return argparse.ArgumentParser(formatter_class=aadhf)
+
+
+def neuro_arg_parser():
+    """
+    Create an argparse.ArgumentParser for neuro environments
+    """
+    parser = arg_parser()
+    parser.add_argument('--num_insts',
+                        help='number of instances to run',
+                        type=int, default=1)
+    parser.add_argument('--pass_reward',
+                        help='whether to pass the prev. reward with obs',
+                        type=bool, default=True)
+    parser.add_argument('--pass_action',
+                        help='whether to pass the prev. action with obs',
+                        type=bool, default=True)
+    parser.add_argument('--net_type', help='type of architecture',
+                        type=str, default='twin_net')
+    parser.add_argument('--num_u',
+                        help='number of total units',
+                        type=int, nargs='+', default=(32,))
+    parser.add_argument('--n_steps',
+                        help='rollout',
+                        type=float, nargs='+', default=(20,))
+    parser.add_argument('--bl_dur',
+                        help='dur. of block in the trial-hist wrappr (trials)',
+                        type=int, nargs='+', default=(200,))
+    parser.add_argument('--stimEv', help='allows scaling stimulus evidence',
+                        type=float, nargs='+', default=(1.,))
+    return parser
+
+
+def main(args):
+    arg_parser = neuro_arg_parser()
+    args, unknown_args = arg_parser.parse_known_args(args)
+    pass_reward = args.pass_reward
+    pass_action = args.pass_action
+    bl_dur = args.bl_dur
+    num_units = args.num_u
+    net_type = args.net_type
+    num_steps_env = 1e7  # [1e9]
+    stim_ev = args.stimEv
+    batch_size = args.n_steps
+    insts = np.arange(args.num_insts)
+    load_path = ''  # '/home/linux/00010'
+    params_config = itertools.product(insts, bl_dur, num_units, stim_ev,
+                                      batch_size)
+    batch_command = ''
+    for conf in params_config:
+        print('---------------------------------------------')
+        cmmd, _ = build_command(ps_r=pass_reward,
+                                ps_act=pass_action,
+                                bl_dur=conf[1], num_u=conf[2],
+                                net_type=net_type, num_stps_env=num_steps_env,
+                                load_path=load_path, stimEv=conf[3],
+                                nsteps=conf[4])
+        batch_command += cmmd + '\n'
+    os.system(batch_command)
+
+
+if __name__ == '__main__':
+
+    # DUAL TASK
+    project = 'dual_task'
+    cluster = 'bsc'
+    # main_folder = '/rigel/theory/users/mm5514/'
+    main_folder = '/gpfs/projects/hcli64/molano/priors/'
+    scripts_folder = home + '/' + project + '/' + cluster + '_scripts/'
+    if not os.path.exists(scripts_folder):
+        os.makedirs(scripts_folder)
+    hours = '8'
+    alg = ['supervised']
+    num_units = [16]
+    bl_dur = [200]
+    stim_ev = [.5]
+    batch_size = [20]
+    net_type = ['cont_rnn']
+    pass_r = [True]
+    pass_act = [True]
+    combine = [True]
+    num_insts = 300
+    num_steps_env = 1e8
+    experiment = '16_neurons_100_instances_longer'
+
+    produce_sh_files(cluster=cluster, alg=alg, hours=hours,
+                     num_units=num_units,
+                     bl_dur=bl_dur, stim_ev=stim_ev, rep_prob=rep_prob,
+                     batch_size=batch_size, net_type=net_type,
+                     pass_r=pass_r, pass_act=pass_act,
+                     num_insts=num_insts, experiment=experiment,
+                     main_folder=main_folder, num_steps_env=num_steps_env)
+    command += 'sbatch ' + experiment + '/analysis_' + cluster + '.sh\n'
--- ops/test_new.py
+++ ops/test_new.py
@@ -1,95 +1,169 @@
-
+import matplotlib
 import numpy as np
 import sys
-import os
 from os.path import expanduser
 home = expanduser("~")
-print(sys.path)
-print(home)
-print('--------------')
 sys.path.append(home + '/neurogym')
 sys.path.append(home + '/gym')
-# print(os.environ['PYTHONPATH'].split(os.pathsep))
-print(sys.path)
 import gym
 from neurogym.wrappers import trial_hist
 from neurogym.wrappers import reaction_time
 from neurogym.wrappers import combine
 from neurogym.wrappers import pass_reward
+from neurogym.wrappers import pass_action
 from neurogym.wrappers import manage_data as md
-
-import sys
 from neurogym.ops import task_registrations
-import matplotlib
 display_mode = True
 if display_mode:
     matplotlib.use('Qt5Agg')
 else:
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-dt = 80
-# calling from the terminal (ploting mode ON)
-# example              task   num. steps                wrapper              pass_reward        plot
-# python  test_new.py RDM-v0    400        trial_hist/reaction_time/combine   True/False    True/False
-if len(sys.argv) > 1:
-    params = {'task': sys.argv[1], 'num_steps': sys.argv[2],
-              'wrapper': sys.argv[3], 'pass_reward': sys.argv[4],
-              'plot': sys.argv[5]}
-else:
-    params = {'task': 'RDM-v0', 'num_steps': 1000, 'wrapper': '',
-              'pass_reward': 'True', 'plot': 'False'}
 
-task_registrations.register_neuroTask(params['task'])
-# task
-env = gym.make(params['task'], **{'dt': dt})
-# wrappers
-if params['wrapper'] == 'trial_hist':
-    env = trial_hist.TrialHistory(env)
-elif params['wrapper'] == 'reaction_time':
-    env = reaction_time.ReactionTime(env)
-elif params['wrapper'] == 'combine':
-    env_extra = gym.make('GNG-v0', **{'dt': dt})
-    # delay is in ms
-    env = combine.combine(dt=dt, env1=env, env2=env_extra, delay=200)
 
-if params['pass_reward'] == 'True':
-    env = pass_reward.PassReward(env)
+def arg_parser():
+    """
+    Create an empty argparse.ArgumentParser.
+    """
+    import argparse
+    aadhf = argparse.ArgumentDefaultsHelpFormatter
+    return argparse.ArgumentParser(formatter_class=aadhf)
 
-# save/render data wrapper
-env = md.manage_data(env, plt_tr=False)
-env.seed(0)
-env.action_space.seed(0)
-env.reset()
-observations = []
-rewards = []
-actions = []
-actions_end_of_trial = []
-for stp in range(int(params['num_steps'])):
-    action = env.action_space.sample()
-    obs, rew, done, info = env.step(action)
-    if done:
-        env.reset()
-    observations.append(obs)
-    if 'new_trial' in info.keys():
-        actions_end_of_trial.append(action)
-    else:
-        actions_end_of_trial.append(-1)
-    rewards.append(rew)
-    actions.append(action)
 
-if params['plot'] == 'True':
-    obs = np.array(observations)
-    plt.figure()
-    plt.subplot(3, 1, 1)
-    plt.imshow(obs.T, aspect='auto')
-    plt.title('observations')
-    plt.subplot(3, 1, 2)
-    plt.plot(actions, marker='+')
-    plt.plot(actions_end_of_trial, '--')
-    plt.title('actions')
-    plt.xlim([0, len(rewards)])
-    plt.subplot(3, 1, 3)
-    plt.plot(rewards, 'r')
-    plt.title('reward')
-    plt.xlim([0, len(rewards)])
-    plt.show()
+def neuro_arg_parser():
+    """
+    Create an argparse.ArgumentParser for neuro environments
+    """
+    parser = arg_parser()
+    parser.add_argument('--pass_reward', '-pr',
+                        help='whether to pass the prev. reward with obs',
+                        type=bool, default=False)
+    parser.add_argument('--pass_action', '-pa',
+                        help='whether to pass the prev. action with obs',
+                        type=bool, default=False)
+    parser.add_argument('--combine',
+                        help='whether to combine with another task',
+                        type=bool, default=False)
+    parser.add_argument('--trial_hist',
+                        help='whether to introduce trial history',
+                        type=bool, default=False)
+    parser.add_argument('--reaction_time', '-rt',
+                        help='whether to make the task a reaction-time task',
+                        type=bool, default=False)
+    parser.add_argument('--task1', help='primary task',
+                        type=str, default='RDM-v0')
+    parser.add_argument('--task2', help='secondary task (if combine is True)',
+                        type=str, default='GNG-v0')
+    parser.add_argument('--n_steps',
+                        help='number of steps',
+                        type=int, default=400)
+    parser.add_argument('--delay',
+                        help='delay with which task2 will start',
+                        type=int, default=500)
+    parser.add_argument('--bl_dur',
+                        help='dur. of block in the trial-hist wrappr (trials)',
+                        type=int, nargs='+', default=(200,))
+    parser.add_argument('--stimEv', help='allows scaling stimulus evidence',
+                        type=float, nargs='+', default=(1.,))
+    parser.add_argument('--plot',
+                        help='show figure',
+                        type=bool, default=True)
+    return parser
+
+
+def main(args):
+    arg_pars = neuro_arg_parser()
+    args, unknown_args = arg_pars.parse_known_args(args)
+    task1 = args.task1
+    pass_rew = args.pass_reward
+    pass_act = args.pass_action
+    tr_hist = args.trial_hist
+    react_t = args.reaction_time
+    comb = args.combine
+    if combine:
+        task2 = args.task2
+        delay = args.delay
+    plot_fig = args.plot
+    num_steps_env = args.n_steps  # [1e9]
+    dt = 100
+
+    task_registrations.register_neuroTask(task1)
+    # task
+    print('Making ' + task1 + ' task')
+    env = gym.make(task1, **{'dt': dt})  # TODO: add params for task
+    # wrappers
+    print('xxxxxx')
+    print('Wrappers')
+    if tr_hist:
+        print('trial history')
+        env = trial_hist.TrialHistory(env)
+    if react_t:
+        print('reaction time')
+        env = reaction_time.ReactionTime(env)
+    if comb:
+        print('combine with ' + task2)
+        task_registrations.register_neuroTask(task2)
+        env_extra = gym.make(task2, **{'dt': dt})  # TODO: add params for task
+        # delay is in ms
+        env = combine.combine(dt=dt, env1=env, env2=env_extra, delay=delay)
+    if pass_rew:
+        print('pass reward')
+        env = pass_reward.PassReward(env)
+    if pass_act:
+        print('pass action')
+        env = pass_action.PassAction(env)
+
+    # save/render data wrapper
+    env = md.manage_data(env, plt_tr=False)
+    env.seed(0)
+    env.action_space.seed(0)
+    observations = []
+    rewards = []
+    actions = []
+    actions_end_of_trial = []
+    gt = []
+    config_mat = []
+    for stp in range(int(num_steps_env)):
+        action = 1  # env.action_space.sample()
+        obs, rew, done, info = env.step(action)
+        if done:
+            env.reset()
+        observations.append(obs)
+        if info['new_trial']:
+            actions_end_of_trial.append(action)
+        else:
+            actions_end_of_trial.append(-1)
+        rewards.append(rew)
+        actions.append(action)
+        gt.append(info['gt'])
+        if 'config' in info.keys():
+            config_mat.append(info['config'])
+        else:
+            config_mat.append([0, 0])
+
+    if plot_fig:
+        rows = 4
+        obs = np.array(observations)
+        plt.figure()
+        plt.subplot(rows, 1, 1)
+        plt.imshow(obs.T, aspect='auto')
+        plt.title('observations')
+        plt.subplot(rows, 1, 2)
+        config_mat = np.array(config_mat)
+        plt.imshow(config_mat.T, aspect='auto')
+        plt.subplot(rows, 1, 3)
+        plt.plot(actions, marker='+')
+        plt.plot(actions_end_of_trial, '--')
+        gt = np.array(gt)
+        plt.plot(np.argmax(gt, axis=1), 'r')
+        plt.title('actions')
+        plt.xlim([0, len(rewards)])
+        plt.subplot(rows, 1, 4)
+        plt.plot(rewards, 'r')
+        plt.title('reward')
+        plt.xlim([0, len(rewards)])
+        plt.show()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
