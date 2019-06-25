ea1360bf7987ed2bd08fa54c90cfe90a48b627f1
Molano <molano@ibp10333.csc.es>
2019-06-25 18:03:46 +0200

    reorganize and simplify pd-wager task

--- envs/pd_wager.py
+++ envs/pd_wager.py
@@ -15,24 +15,22 @@ Perceptual decision-making with postdecision wagering, based on
 
 """
 from __future__ import division
-import ngym
+from neurogym.ops import tasktools
+from neurogym.envs import ngym
 from gym import spaces
 import numpy as np
 
-import tasktools
-
 
 class PDWager(ngym.ngym):
-    def __init__(self, dt=100):
+    def __init__(self, dt=100, timing=(750, 100, 180, 800, 1200, 1350,
+                                       1800, 500, 575, 750, 500)):
         # call ngm __init__ function
         super().__init__(dt=dt)
-        # Inputs
-        self.inputs = tasktools.to_map('FIXATION', 'LEFT', 'RIGHT', 'SURE')
-
-        # Actions
-        self.actions = tasktools.to_map('FIXATE', 'CHOOSE-LEFT',
-                                        'CHOOSE-RIGHT', 'CHOOSE-SURE')
-
+#        # Actions
+#        self.actions = tasktools.to_map('FIXATE', 'CHOOSE-LEFT',
+#                                        'CHOOSE-RIGHT', 'CHOOSE-SURE')
+        # Actions (fixate, left, right, sure)
+        self.actions = [0, -1, 1, 2]
         # trial conditions
         self.wagers = [True, False]
         self.choices = [-1, 1]
@@ -42,26 +40,31 @@ class PDWager(ngym.ngym):
         self.sigma = np.sqrt(2*100*0.01)
 
         # Durations
-        self.fixation = 750
-        self.stimulus_min = 100
-        self.stimulus_mean = 180
-        self.stimulus_max = 800
-        self.delay_min = 1200
-        self.delay_mean = 1350
-        self.delay_max = 1800
-        self.sure_min = 500
-        self.sure_mean = 575
-        self.sure_max = 750
-        self.decision = 500
+        self.fixation = timing[0]  # 750
+        self.stimulus_min = timing[1]  # 100
+        self.stimulus_mean = timing[2]  # 180
+        self.stimulus_max = timing[3]  # 800
+        self.delay_min = timing[4]  # 1200
+        self.delay_mean = timing[5]  # 1350
+        self.delay_max = timing[6]  # 1800
+        self.sure_min = timing[7]  # 500
+        self.sure_mean = timing[8]  # 575
+        self.sure_max = timing[9]  # 750
+        self.decision = timing[10]  # 500
         self.mean_trial_duration = self.fixation + self.stimulus_mean +\
             self.delay_mean + self.decision
+        if self.fixation == 0 or self.decision == 0 or self.stimulus_mean == 0:
+            print('XXXXXXXXXXXXXXXXXXXXXX')
+            print('the duration of all periods must be larger than 0')
+            print('XXXXXXXXXXXXXXXXXXXXXX')
         print('mean trial duration: ' + str(self.mean_trial_duration) +
-              ' (max num. steps: ' + str(self.mean_trial_duration/self.dt) +
-              ')')
+              ' (max num. steps: ' +
+              str(self.mean_trial_duration/self.dt) + ')')
         # Rewards
         self.R_ABORTED = -0.1
         self.R_CORRECT = +1.
         self.R_MISS = 0.
+        self.R_FAIL = 0.
         self.abort = False
         self.R_SURE = 0.7*self.R_CORRECT
 
@@ -99,16 +102,15 @@ class PDWager(ngym.ngym):
                                                 self.delay_mean,
                                                 xmin=self.delay_min,
                                                 xmax=self.delay_max)
-
+        # maximum duration of current trial
+        self.tmax = self.fixation + stimulus + delay + self.decision
         if wager:
             sure_onset =\
                 tasktools.truncated_exponential(self.rng, self.dt,
                                                 self.sure_mean,
                                                 xmin=self.sure_min,
                                                 xmax=self.sure_max)
-        # maximum duration of current trial
-        self.tmax = self.fixation + stimulus + delay + self.decision
-        # TODO: are these periods correct?
+
         durations = {
             'fixation':  (0, self.fixation),
             'stimulus':  (self.fixation, self.fixation + stimulus),
@@ -139,83 +141,66 @@ class PDWager(ngym.ngym):
     def _step(self, action):
         trial = self.trial
         # ---------------------------------------------------------------------
-        # Reward
+        # Reward and inputs
         # ---------------------------------------------------------------------
-
-        # epochs = trial['epochs']
+        trial = self.trial
         info = {'new_trial': False}
+        # ground truth signal is not well defined in this task
+        info['gt'] = np.zeros((4,))
+        # rewards
         reward = 0
-        tr_perf = False
+        # observations
+        obs = np.zeros((4,))
         if self.in_epoch(self.t, 'fixation'):
-            if (action != self.actions['FIXATE']):
+            obs[0] = 1
+            if self.actions[action] != 0:
                 info['new_trial'] = self.abort
                 reward = self.R_ABORTED
         if self.in_epoch(self.t, 'decision'):
-            if action == self.actions['CHOOSE-LEFT']:
-                tr_perf = True
-                info['new_trial'] = True
-                if (trial['ground_truth'] < 0):
-                    reward = self.R_CORRECT
-            elif action == self.actions['CHOOSE-RIGHT']:
-                tr_perf = True
-                info['new_trial'] = True
-                if (trial['ground_truth'] > 0):
-                    reward = self.R_CORRECT
-            elif action == self.actions['CHOOSE-SURE']:
-                tr_perf = True
-                info['new_trial'] = True
+            if self.actions[action] == 2:
                 if trial['wager']:
                     reward = self.R_SURE
                 else:
                     reward = self.R_ABORTED
+            else:
+                gt_sign = np.sign(trial['ground_truth'])
+                action_sign = np.sign(self.actions[action])
+                if gt_sign == action_sign:
+                    reward = self.R_CORRECT
+                elif gt_sign == -action_sign:
+                    reward = self.R_FAIL
+            info['new_trial'] = self.actions[action] != 0
 
-        # ---------------------------------------------------------------------
-        # Inputs
-        # ---------------------------------------------------------------------
-
-        if trial['ground_truth'] < 0:
-            high = self.inputs['LEFT']
-            low = self.inputs['RIGHT']
-        else:
-            high = self.inputs['RIGHT']
-            low = self.inputs['LEFT']
-
-        obs = np.zeros(len(self.inputs))
-        if (self.in_epoch(self.t, 'fixation') or
-           self.in_epoch(self.t, 'stimulus') or
-                self.in_epoch(self.t, 'delay')):
-            obs[self.inputs['FIXATION']] = 1
-        if self.in_epoch(self.t, 'stimulus'):
+        if self.in_epoch(self.t, 'delay'):
+            obs[0] = 1
+        elif self.in_epoch(self.t, 'stimulus'):
+            high = (trial['ground_truth'] > 0) + 1
+            low = (trial['ground_truth'] < 0) + 1
             obs[high] = self.scale(+trial['coh']) +\
-                self.rng.normal(scale=self.sigma)/np.sqrt(self.dt)
+                self.rng.gauss(mu=0, sigma=self.sigma)/np.sqrt(self.dt)
             obs[low] = self.scale(-trial['coh']) +\
-                self.rng.normal(scale=self.sigma)/np.sqrt(self.dt)
+                self.rng.gauss(mu=0, sigma=self.sigma)/np.sqrt(self.dt)
         if trial['wager'] and self.in_epoch(self.t, 'sure'):
-            obs[self.inputs['SURE']] = 1
+            obs[3] = 1
 
         # ---------------------------------------------------------------------
         # new trial?
-        reward, new_trial = tasktools.new_trial(self.t, self.tmax, self.dt,
-                                                info['new_trial'],
-                                                self.R_MISS, reward)
+        reward, info['new_trial'] = tasktools.new_trial(self.t, self.tmax,
+                                                        self.dt,
+                                                        info['new_trial'],
+                                                        self.R_MISS, reward)
 
-        if new_trial:
-            info['new_trial'] = True
-            info['gt'] = trial['ground_truth']
+        if info['new_trial']:
             self.t = 0
             self.num_tr += 1
-            # compute perf
-            self.perf, self.num_tr_perf =\
-                tasktools.compute_perf(self.perf, reward,
-                                       self.num_tr_perf, tr_perf)
         else:
             self.t += self.dt
 
         done = self.num_tr > self.num_tr_exp
-        return obs, reward, done, info, new_trial
+        return obs, reward, done, info
 
     def step(self, action):
-        obs, reward, done, info, new_trial = self._step(action)
-        if new_trial:
+        obs, reward, done, info = self._step(action)
+        if info['new_trial']:
             self.trial = self._new_trial()
         return obs, reward, done, info
