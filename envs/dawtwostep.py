c15a795c726e591ff5881db086877751b681916b
Molano <molano@ibp10333.csc.es>
2019-06-26 18:31:04 +0200

    change two-step task

--- envs/dawtwostep.py
+++ envs/dawtwostep.py
@@ -8,30 +8,25 @@ from __future__ import division
 
 import numpy as np
 from gym import spaces
-import tasktools
-import ngym
+from neurogym.ops import tasktools
+from neurogym.envs import ngym
 
 
 class DawTwoStep(ngym.ngym):
     def __init__(self, dt=100):
         super().__init__(dt=dt)
-        # TODO: remain to be carefully tested
-        # Inputs
-        self.inputs = tasktools.to_map('FIXATION', 'STATE1', 'STATE2')
-
-        # Actions
-        self.actions = tasktools.to_map('FIXATE', 'ACTION1', 'ACTION2')
+        # Actions ('FIXATE', 'ACTION1', 'ACTION2')
+        self.actions = [0, 1, 2]
 
         # trial conditions
-        self.p1 = 0.8  # first stage transition probability
-        self.p2 = 0.8  # second stage transition probability
-        # TODO: this should be implemented through the wrapper
+        self.p1 = 0.8  # probability of transitioning to state1 with action1
+        self.p2 = 0.8  # probability of transitioning to state2 with action2
         self.p_switch = 0.025  # switch reward contingency
         self.high_reward_p = 0.9
         self.low_reward_p = 0.1
-        self.state1_high_reward = True
-        self.tmax = 2
+        self.tmax = 3*self.dt
         self.mean_trial_duration = self.tmax
+        self.state1_high_reward = self.rng.random() > 0.5
         print('mean trial duration: ' + str(self.mean_trial_duration) +
               ' (max num. steps: ' + str(self.mean_trial_duration/self.dt) +
               ')')
@@ -43,7 +38,6 @@ class DawTwoStep(ngym.ngym):
         self.R_CORRECT = +1.
         self.R_FAIL = 0.
         self.R_MISS = 0.
-        self.abort = False
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(-np.inf, np.inf, shape=(3,),
                                             dtype=np.float32)
@@ -59,24 +53,29 @@ class DawTwoStep(ngym.ngym):
         # ---------------------------------------------------------------------
 
         # determine the transitions
-        transition = dict()
-        st1 = self.inputs['STATE1']
-        st2 = self.inputs['STATE2']
-        tmp1 = st1 if self.rng.rand() < self.p1 else st2
-        tmp2 = st2 if self.rng.rand() < self.p1 else st1
-        transition[self.actions['ACTION1']] = tmp1
-        transition[self.actions['ACTION2']] = tmp2
+        transition = np.empty((3,))
+        st1 = 1
+        st2 = 2
+        tmp1 = st1 if self.rng.random() < self.p1 else st2
+        tmp2 = st2 if self.rng.random() < self.p2 else st1
+        transition[self.actions[1]] = tmp1
+        transition[self.actions[2]] = tmp2
 
+        # swtich reward contingency
+        switch = self.rng.random() < self.p_switch
+        if switch:
+            self.state1_high_reward = not self.state1_high_reward
+        # which state to reward with more probability
         if self.state1_high_reward:
-            hi_state, low_state = 'STATE1', 'STATE2'
+            hi_state, low_state = 0, 1
         else:
-            hi_state, low_state = 'STATE2', 'STATE1'
+            hi_state, low_state = 1, 0
 
-        reward = dict()
-        reward[self.inputs[hi_state]] =\
-            (self.rng.rand() < self.high_reward_p) * self.R_CORRECT
-        reward[self.inputs[low_state]] =\
-            (self.rng.rand() < self.low_reward_p) * self.R_CORRECT
+        reward = np.empty((2,))
+        reward[hi_state] = (self.rng.random() <
+                            self.high_reward_p) * self.R_CORRECT
+        reward[low_state] = (self.rng.random() <
+                             self.low_reward_p) * self.R_CORRECT
 
         return {
             'transition':  transition,
@@ -85,53 +84,47 @@ class DawTwoStep(ngym.ngym):
 
     def _step(self, action):
         trial = self.trial
-        info = {'new_trial': False}
+        info = {'new_trial': False, 'gt': np.zeros((3,))}
         reward = 0
-        tr_perf = False
 
-        # TODO: should we section by reward/input or epochs?
-        obs = np.zeros(len(self.inputs))
-        if self.t == 0:  # at stage 1
-            if action == self.actions['FIXATE']:
+        obs = np.zeros((3,))
+        if self.t == 0:  # at stage 1, if action==fixate, abort
+            if action == 0:
                 reward = self.R_ABORTED
-                info['new_trial'] = self.abort
+                info['new_trial'] = True
             else:
                 state = trial['transition'][action]
-                obs[state] = 1
-                reward = trial['reward'][state]
-        elif self.t == 1:
-            obs[self.inputs['FIXATION']] = 1
-            if action != self.actions['FIXATE']:
+                obs[int(state)] = 1
+                reward = trial['reward'][int(state-1)]
+        elif self.t == self.dt:
+            obs[0] = 1
+            if action != 0:
                 reward = self.R_ABORTED
-            else:
-                tr_perf = True
             info['new_trial'] = True
         else:
             raise ValueError('t is not 0 or 1')
 
         # ---------------------------------------------------------------------
         # new trial?
-        reward, new_trial = tasktools.new_trial(self.t, self.tmax, self.dt,
-                                                info['new_trial'],
-                                                self.R_MISS, reward)
-
-        # TODO: This is redundant, because it's moved to step
-        if new_trial:
-            info['new_trial'] = True
+        reward, info['new_trial'] = tasktools.new_trial(self.t, self.tmax,
+                                                        self.dt,
+                                                        info['new_trial'],
+                                                        self.R_MISS, reward)
+        if info['new_trial']:
             self.t = 0
             self.num_tr += 1
-            # compute perf
-            self.perf, self.num_tr_perf =\
-                tasktools.compute_perf(self.perf, reward,
-                                       self.num_tr_perf, tr_perf)
         else:
-            self.t += 1
+            self.t += self.dt
 
         done = self.num_tr > self.num_tr_exp
-        return obs, reward, done, info, new_trial
+        print('-----------------------')
+        print(self.t)
+        print(reward)
+        print('-----------------------')
+        return obs, reward, done, info
 
     def step(self, action):
-        obs, reward, done, info, new_trial = self._step(action)
-        if new_trial:
+        obs, reward, done, info = self._step(action)
+        if info['new_trial']:
             self.trial = self._new_trial()
         return obs, reward, done, info
