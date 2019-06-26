91f8d6ed513a3b7c0baf5ba89df0474fee691dc1
Molano <molano@ibp10333.csc.es>
2019-06-26 15:49:06 +0200

    simplify delay-match sample task

--- envs/delaymatchsample.py
+++ envs/delaymatchsample.py
@@ -8,34 +8,35 @@ from __future__ import division
 
 import numpy as np
 from gym import spaces
-import tasktools
-import ngym
+from neurogym.ops import tasktools
+from neurogym.envs import ngym
 
 
 class DelayedMatchToSample(ngym.ngym):
-    def __init__(self, dt=100):
+    def __init__(self, dt=100, timing=(500, 500, 1500, 500, 500)):
         super().__init__(dt=dt)
-        # Inputs
         # TODO: Code a continuous space version
-        self.inputs = tasktools.to_map('FIXATION', 'LEFT', 'RIGHT')
-
-        # Actions
-        self.actions = tasktools.to_map('FIXATE', 'MATCH', 'NONMATCH')
-
+        # Actions ('FIXATE', 'MATCH', 'NONMATCH')
+        self.actions = [0, -1, 1]
         # Input noise
         self.sigma = np.sqrt(2*100*0.01)
 
         # TODO: Find these info from a paper
-        self.tmax = 3500
-        self.fixation = 500
-        self.sample = 500
-        self.delay = 1500
-        self.test = 500
-        self.decision = 500
-        self.mean_trial_duration = self.tmax
-        print('mean trial duration: ' + str(self.mean_trial_duration) +
-              ' (max num. steps: ' + str(self.mean_trial_duration/self.dt) +
-              ')')
+        self.fixation = timing[0]
+        self.sample = timing[1]
+        self.delay = timing[2]
+        self.test = timing[3]
+        self.decision = timing[4]
+        self.tmax = np.sum(timing)
+
+        mean_trial_duration = self.tmax
+        if self.fixation == 0 or self.sample == 0 or self.delay == 0 or\
+           self.test == 0 or self.decision == 0:
+            print('XXXXXXXXXXXXXXXXXXXXXX')
+            print('the duration of all periods must be larger than 0')
+            print('XXXXXXXXXXXXXXXXXXXXXX')
+        print('mean trial duration: ' + str(mean_trial_duration) +
+              ' (max num. steps: ' + str(mean_trial_duration/self.dt) + ')')
         # Rewards
         self.R_ABORTED = -0.1
         self.R_CORRECT = +1.
@@ -56,30 +57,27 @@ class DelayedMatchToSample(ngym.ngym):
         # ---------------------------------------------------------------------
         # Epochs
         # ---------------------------------------------------------------------
-
-        # TODO: this is a lot of repeated typing
         dur = {'tmax': self.tmax}
         dur['fixation'] = (0, self.fixation)
         dur['sample'] = (dur['fixation'][1], dur['fixation'][1] + self.sample)
         dur['delay'] = (dur['sample'][1], dur['sample'][1] + self.delay)
         dur['test'] = (dur['delay'][1], dur['delay'][1] + self.test)
         dur['decision'] = (dur['test'][1], dur['test'][1] + self.decision)
-
         # ---------------------------------------------------------------------
         # Trial
         # ---------------------------------------------------------------------
 
         # TODO: may need to fix this
-        match = tasktools.choice(self.rng, ['MATCH', 'NONMATCH'])
-        sample = tasktools.choice(self.rng, ['LEFT', 'RIGHT'])
-        if match == 'MATCH':
+        gt = tasktools.choice(self.rng, [-1, 1])
+        sample = tasktools.choice(self.rng, [0, 1])
+        if gt == 1:
             test = sample
         else:
-            test = 'LEFT' if sample == 'RIGHT' else 'RIGHT'
+            test = 1*(not sample)
 
         return {
             'durations': dur,
-            'match': match,
+            'ground_truth': gt,
             'sample': sample,
             'test': test,
             }
@@ -89,55 +87,53 @@ class DelayedMatchToSample(ngym.ngym):
         # Reward
         # ---------------------------------------------------------------------
         trial = self.trial
-        info = {'new_trial': False}
+        info = {'new_trial': False, 'gt': np.zeros((3,))}
         reward = 0
-        tr_perf = False
+        obs = np.zeros((3,))
+
         if self.in_epoch(self.t, 'fixation'):
-            if (action != self.actions['FIXATE']):
+            obs[0] = 1
+            if self.actions[action] != 0:
                 info['new_trial'] = self.abort
                 reward = self.R_ABORTED
         if self.in_epoch(self.t, 'decision'):
-            if action != self.actions['FIXATE']:
-                tr_perf = True
-                info['new_trial'] = True
-                if (action == self.actions[trial['match']]):
-                    reward = self.R_CORRECT
-                else:
-                    reward = self.R_FAIL
+            info['gt'][int((trial['ground_truth']/2+1.5))] = 1
+            gt_sign = np.sign(trial['ground_truth'])
+            action_sign = np.sign(self.actions[action])
+            if gt_sign == action_sign:
+                reward = self.R_CORRECT
+            elif gt_sign == -action_sign:
+                reward = self.R_FAIL
+            info['new_trial'] = self.actions[action] != 0
+        else:
+            info['gt'][0] = 1
 
         # ---------------------------------------------------------------------
         # Inputs
         # ---------------------------------------------------------------------
-        obs = np.zeros(len(self.inputs))
-        if not self.in_epoch(self.t, 'decision'):
-            obs[self.inputs['FIXATION']] = 1
         if self.in_epoch(self.t, 'sample'):
-            obs[self.inputs[trial['sample']]] = 1
+            obs[trial['sample']+1] = 1
         if self.in_epoch(self.t, 'test'):
-            obs[self.inputs[trial['test']]] = 1
+            obs[trial['test']+1] = 1
 
         # ---------------------------------------------------------------------
         # new trial?
-        reward, new_trial = tasktools.new_trial(self.t, self.tmax, self.dt,
-                                                info['new_trial'],
-                                                self.R_MISS, reward)
+        reward, info['new_trial'] = tasktools.new_trial(self.t, self.tmax,
+                                                        self.dt,
+                                                        info['new_trial'],
+                                                        self.R_MISS, reward)
 
-        if new_trial:
-            info['new_trial'] = True
+        if info['new_trial']:
             self.t = 0
             self.num_tr += 1
-            # compute perf
-            self.perf, self.num_tr_perf =\
-                tasktools.compute_perf(self.perf, reward,
-                                       self.num_tr_perf, tr_perf)
         else:
             self.t += self.dt
 
         done = self.num_tr > self.num_tr_exp
-        return obs, reward, done, info, new_trial
+        return obs, reward, done, info
 
     def step(self, action):
-        obs, reward, done, info, new_trial = self._step(action)
-        if new_trial:
+        obs, reward, done, info = self._step(action)
+        if info['new_trial']:
             self.trial = self._new_trial()
         return obs, reward, done, info
