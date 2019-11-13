841786496c9b48470cd54ae54d2281253180b6c9
Molano <molano@ibp10333.csc.es>
2019-11-13 12:39:15 +0100

    control number of times per side

--- wrappers/cv_learning.py
+++ wrappers/cv_learning.py
@@ -26,7 +26,7 @@ class CurriculumLearning(Wrapper):
         self.curr_ph = 0
         self.curr_perf = 0
         self.perf_window = perf_w
-        self.counter = []  # TIP: could this be just a scalar?
+        self.counter = 0
 
     def _set_trial_params(self):
         # ---------------------------------------------------------------------
@@ -38,7 +38,7 @@ class CurriculumLearning(Wrapper):
             self.task.stimulus_mean = 0
             self.task.stimulus_min = 0
             self.task.stimulus_max = 0
-            self.task.delays
+            # self.task.delays
             self.task.R_FAIL = self.task.R_CORRECT
         elif self.curr_ph == 1:
             # there is stim but first answer is not penalized
@@ -57,39 +57,26 @@ class CurriculumLearning(Wrapper):
         elif self.curr_ph == 4:
             self.task.coh = np.array([0, 6.4, 12.8, 25.6, 51.2]) *\
                 self.task.stimEv
-        # TIP: you don't need to return this the task is modified and
-        # can be access via self.task
-        # return self.task
 
-    def counter(self, action):
+    def count(self, action):
         # analyzes the last three answers during stage 0
-        self.alternate = False
-        if self.curr_ph == 0:
-            self.counter.append(self.task.actions[action])
-            if len(self.counter) > 3:
-                del self.counter[0]
-            if abs(sum(self.counter)) == 3:
-                self.alternate = True
-        # return self.alternate
-
-    def _alternate(self, action):
-        action_sign = np.sign(self.actions[action])
-        self.task.R_CORRECT = self.task.R_FAIL =\
-            abs(self.counter[0]+action_sign-action_sign*2)/2
-        # return self.task
+        # self.alternate = False
+        new = self.task.actions[action]
+        if np.sign(self.counter) == np.sign(new):
+            self.counter += new
+        else:
+            self.counter = new
 
     def reset(self):
         return self.task.reset()
 
     def step(self, action):
-        # TIP: you might want to put this inside the if below
-        #        if self.alternate:
-        #            self._alternate(action)
         obs, reward, done, info = self.env._step(action)
         if info['new_trial']:
-            # TIP: the following 2 lines are only executed in phase 0, right?
-            self.counter(action)  # TIP: this function takes an action
-            self._alternate(action)
+            if self.curr_ph == 0:
+                self.count(action)
+                if np.abs(self.counter) >= self.max_num_reps:
+                    reward = self.task.FAIL
             self._set_trial_params()
             self.task.trial = self.task._new_trial()
         return obs, reward, done, info
