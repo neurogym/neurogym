1a22be14bc73ad4da13d852345b8739e9998baaa
Molano <molano@ibp10333.csc.es>
2019-11-14 12:37:25 +0100

    n-alternative rdm

---
+++ envs/nalt_rdm.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 17:39:17 2019
+
+@author: molano
+
+
+Perceptual decision-making task, based on
+
+  Bounded integration in parietal cortex underlies decisions even when viewing
+  duration is dictated by the environment.
+  R Kiani, TD Hanks, & MN Shadlen, JNS 2008.
+
+  http://dx.doi.org/10.1523/JNEUROSCI.4761-07.2008
+
+  But allowing for more than 2 choices.
+
+"""
+from __future__ import division
+
+import numpy as np
+from gym import spaces
+from neurogym.ops import tasktools
+from neurogym.envs import ngym
+import matplotlib.pyplot as plt
+
+
+class N_AltRDM(ngym.ngym):
+    def __init__(self, dt=100, timing=(500, 80, 330, 1500, 500), n=3,
+                 stimEv=1., **kwargs):
+        super().__init__(dt=dt)
+        self.n = n
+        # Actions (fixate, left, right)
+        self.actions = np.arange(n+1)
+        # trial conditions (left, right)
+        self.choices = np.arange(1, n+1)
+        # cohs specifies the amount of evidence (which is modulated by stimEv)
+        self.cohs = np.array([6.4, 12.8, 25.6, 51.2])*10
+        # Input noise
+        self.sigma = np.sqrt(2*100*0.01)
+        # Durations (stimulus duration will be drawn from an exponential)
+        self.fixation = timing[0]
+        self.stimulus_min = timing[1]
+        self.stimulus_mean = timing[2]
+        self.stimulus_max = timing[3]
+        self.decision = timing[4]
+        self.mean_trial_duration = self.fixation + self.stimulus_mean +\
+            self.decision
+        if self.fixation == 0 or self.decision == 0 or self.stimulus_mean == 0:
+            print('XXXXXXXXXXXXXXXXXXXXXX')
+            print('the duration of all periods must be larger than 0')
+            print('XXXXXXXXXXXXXXXXXXXXXX')
+        print('XXXXXXXXXXXXXXXXXXXXXX')
+        print('Random Dots Motion Task')
+        print('Fixation: ' + str(self.fixation))
+        print('Min Stimulus Duration: ' + str(self.stimulus_min))
+        print('Mean Stimulus Duration: ' + str(self.stimulus_mean))
+        print('Max Stimulus Duration: ' + str(self.stimulus_max))
+        print('Decision: ' + str(self.decision))
+        print('(time step: ' + str(self.dt) + ')')
+        print('XXXXXXXXXXXXXXXXXXXXXX')
+        # Rewards
+        self.R_ABORTED = -0.1
+        self.R_CORRECT = +1.
+        self.R_FAIL = 0.
+        self.R_MISS = 0.
+        self.abort = False
+        # action and observation spaces
+        self.stimulus_min = np.max([self.stimulus_min, dt])
+        self.action_space = spaces.Discrete(n+1)
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(n+1,),
+                                            dtype=np.float32)
+        # seeding
+        self.seed()
+        self.viewer = None
+
+        # start new trial
+        self.trial = self._new_trial()
+
+    def _new_trial(self):
+        """
+        _new_trial() is called when a trial ends to get the specifications of
+        the next trial. Such specifications are stored in a dictionary with
+        the following items:
+            durations, which stores the duration of the different periods (in
+            the case of rdm: fixation, stimulus and decision periods)
+            ground truth: correct response for the trial
+            coh: stimulus coherence (evidence) for the trial
+
+        """
+        # ---------------------------------------------------------------------
+        # Epochs
+        # ---------------------------------------------------------------------
+        stimulus = tasktools.truncated_exponential(self.rng, self.dt,
+                                                   self.stimulus_mean,
+                                                   xmin=self.stimulus_min,
+                                                   xmax=self.stimulus_max)
+        # maximum length of current trial
+        self.tmax = self.fixation + stimulus + self.decision
+        durations = {
+            'fixation': (0, self.fixation),
+            'stimulus': (self.fixation, self.fixation + stimulus),
+            'decision': (self.fixation + stimulus,
+                         self.fixation + stimulus + self.decision),
+            }
+
+        # ---------------------------------------------------------------------
+        # Trial
+        # ---------------------------------------------------------------------
+        ground_truth = tasktools.choice(self.rng, self.choices)
+        coh = tasktools.choice(self.rng, self.cohs)
+
+        return {
+            'durations': durations,
+            'ground_truth': ground_truth,
+            'coh': coh
+            }
+
+    # Input scaling
+    def scale(self, coh):
+        return (1 + coh/100)/2
+
+    def _step(self, action):
+        """
+        _step receives an action and returns:
+            a new observation, obs
+            reward associated with the action, reward
+            a boolean variable indicating whether the experiment has end, done
+            a dictionary with extra information:
+                ground truth correct response, info['gt']
+                boolean indicating the end of the trial, info['new_trial']
+        """
+        # ---------------------------------------------------------------------
+        # Reward and observations
+        # ---------------------------------------------------------------------
+        trial = self.trial
+        info = {'new_trial': False}
+        info['gt'] = np.zeros((self.n+1,))
+        # rewards
+        reward = 0
+        # observations
+        obs = np.zeros((self.n+1,))
+        if self.in_epoch(self.t, 'fixation'):
+            info['gt'][0] = 1
+            obs[0] = 1
+            if self.actions[action] != 0:
+                info['new_trial'] = self.abort
+                reward = self.R_ABORTED
+        elif self.in_epoch(self.t, 'decision'):
+            info['gt'][trial['ground_truth']] = 1
+            if self.actions[action] == trial['ground_truth']:
+                reward = self.R_CORRECT
+            elif self.actions[action] != 0:
+                reward = self.R_FAIL
+            info['new_trial'] = self.actions[action] != 0
+        else:
+            info['gt'][0] = 1
+
+        # this is an 'if' to allow the stimulus and fixation periods to overlap
+        if self.in_epoch(self.t, 'stimulus'):
+            obs[0] = 1
+            for ind_obs in range(1, self.n+1):
+                if ind_obs == trial['ground_truth']:
+                    coh = trial['coh']
+                else:
+                    coh = -trial['coh']
+                obs[ind_obs] = self.scale(coh) +\
+                    self.rng.gauss(mu=0, sigma=self.sigma)/np.sqrt(self.dt)
+
+        # ---------------------------------------------------------------------
+        # new trial?
+        reward, info['new_trial'] = tasktools.new_trial(self.t, self.tmax,
+                                                        self.dt,
+                                                        info['new_trial'],
+                                                        self.R_MISS, reward)
+        if info['new_trial']:
+            self.t = 0
+            self.num_tr += 1
+        else:
+            self.t += self.dt
+
+        done = self.num_tr > self.num_tr_exp
+        return obs, reward, done, info
+
+    def step(self, action):
+        """
+        step receives an action and returns:
+            a new observation, obs
+            reward associated with the action, reward
+            a boolean variable indicating whether the experiment has end, done
+            a dictionary with extra information:
+                ground truth correct response, info['gt']
+                boolean indicating the end of the trial, info['new_trial']
+        Note that the main computations are done by the function _step(action),
+        and the extra lines are basically checking whether to call the
+        _new_trial() function in order to start a new trial
+        """
+        obs, reward, done, info = self._step(action)
+        if info['new_trial']:
+            self.trial = self._new_trial()
+        return obs, reward, done, info
+
+
+if __name__ == '__main__':
+    env = N_AltRDM(timing=[100, 200, 200, 200, 100])
+    observations = []
+    rewards = []
+    actions = []
+    actions_end_of_trial = []
+    gt = []
+    config_mat = []
+    num_steps_env = 100
+    for stp in range(int(num_steps_env)):
+        action = env.action_space.sample()
+        obs, rew, done, info = env.step(action)
+        if done:
+            env.reset()
+        observations.append(obs)
+        if info['new_trial']:
+            actions_end_of_trial.append(action)
+        else:
+            actions_end_of_trial.append(-1)
+        rewards.append(rew)
+        actions.append(action)
+        gt.append(info['gt'])
+        if 'config' in info.keys():
+            config_mat.append(info['config'])
+        else:
+            config_mat.append([0, 0])
+
+    rows = 3
+    obs = np.array(observations)
+    plt.figure()
+    plt.subplot(rows, 1, 1)
+    plt.imshow(obs.T, aspect='auto')
+    plt.title('observations')
+    plt.subplot(rows, 1, 2)
+    plt.plot(actions, marker='+')
+    #    plt.plot(actions_end_of_trial, '--')
+    #    gt = np.array(gt)
+    #    plt.plot(np.argmax(gt, axis=1), 'r')
+    #    # aux = np.argmax(obs, axis=1)
+    # aux[np.sum(obs, axis=1) == 0] = -1
+    # plt.plot(aux, '--k')
+    plt.title('actions')
+    plt.xlim([-0.5, len(rewards)+0.5])
+    plt.subplot(rows, 1, 3)
+    plt.plot(rewards, 'r')
+    plt.title('reward')
+    plt.xlim([-0.5, len(rewards)+0.5])
+    plt.show()
