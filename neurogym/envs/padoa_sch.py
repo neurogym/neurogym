3414f8a6c463909e6915e77bb18a2c4397b56637
Molano <molano@ibp10333.csc.es>
2020-01-15 15:14:06 +0100

    minor

--- neurogym/envs/padoa_sch.py
+++ neurogym/envs/padoa_sch.py
@@ -114,19 +114,19 @@ class PadoaSch(ngym.EpochEnv):
     def _step(self, action):
         trial = self.trial
 
-        info = {'new_trial': False}
+        new_trial = False
 
         obs = self.obs_now
 
         reward = 0
         if self.in_epoch('fixation') or self.in_epoch('offer_on'):
             if action != self.actions['FIXATE']:
-                info['new_trial'] = self.abort
+                new_trial = self.abort
                 reward = self.R_ABORTED
         elif self.in_epoch('decision'):
             if action in [self.actions['CHOOSE-LEFT'],
                           self.actions['CHOOSE-RIGHT']]:
-                info['new_trial'] = True
+                new_trial = True
 
                 juiceL, juiceR = trial['juice']
 
@@ -144,4 +144,4 @@ class PadoaSch(ngym.EpochEnv):
                 elif action == self.actions['CHOOSE-RIGHT']:
                     reward = rR
 
-        return obs, reward, False, info
+        return obs, reward, False, {'new_trial': new_trial, 'gt': 0}
