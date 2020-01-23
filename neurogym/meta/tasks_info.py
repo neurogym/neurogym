7ed403e49372ce2ff3f841f5f31da9cbce53214e
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-23 12:30:01 +0100

    move plot_struct function to tasks_info

--- neurogym/meta/tasks_info.py
+++ neurogym/meta/tasks_info.py
@@ -9,9 +9,11 @@ Created on Thu Jan 23 11:04:58 2020
 import gym
 import neurogym as ngym
 from neurogym import all_tasks
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def info(task=None):
+def info(task=None, n_stps_plt=100):
     """Script to get tasks info"""
     if task is None:
         tasks = all_tasks.keys()
@@ -47,12 +49,79 @@ def info(task=None):
                 for key, val in timing.items():
                     dist, args = val
                     string += key + ' : ' + dist + ' ' + str(args) + '\n\n'
+            plot_struct(env, n_stps_plt=n_stps_plt)
         except BaseException as e:
             print('Failure in ', env_name)
             print(e)
         print(string)
 
 
+def plot_struct(env, num_steps_env=200, n_stps_plt=200,
+                def_act=None, model=None, name=''):
+    if isinstance(env, str):
+        env = gym.make(env)
+    # TODO: Move this somewhere else. Shouldn't be in tasktools
+    observations = []
+    rewards = []
+    actions = []
+    actions_end_of_trial = []
+    gt = []
+    config_mat = []
+    perf = []
+    obs = env.reset()
+    for stp in range(int(num_steps_env)):
+        if model is not None:
+            action, _states = model.predict(obs)
+            action = [action]
+        elif def_act is not None:
+            action = def_act
+        else:
+            action = env.action_space.sample()
+        obs, rew, done, info = env.step(action)
+        if isinstance(info, list):
+            info = info[0]
+            obs = obs[0]
+            rew = rew[0]
+            done = done[0]
+        if done:
+            env.reset()
+        observations.append(obs)
+        if info['new_trial']:
+            actions_end_of_trial.append(action)
+            perf.append(rew)
+        else:
+            actions_end_of_trial.append(-1)
+        rewards.append(rew)
+        actions.append(action)
+        gt.append(info['gt'])
+        if 'config' in info.keys():
+            config_mat.append(info['config'])
+        else:
+            config_mat.append([0, 0])
+    print('hey!')
+    rows = 3
+    obs = np.array(observations)
+    plt.figure()
+    plt.subplot(rows, 1, 1)
+    plt.imshow(obs[:n_stps_plt, :].T, aspect='auto')
+    plt.title('observations')
+    plt.subplot(rows, 1, 2)
+    plt.plot(actions[:n_stps_plt], marker='+')
+    gt = np.array(gt)
+    if len(gt.shape) == 2:
+        gt = np.argmax(gt, axis=1)
+    plt.plot(gt[:n_stps_plt], 'r')
+    plt.title('actions')
+    plt.xlim([-0.5, n_stps_plt+0.5])
+    plt.subplot(rows, 1, 3)
+    plt.plot(rewards[:n_stps_plt], 'r')
+    plt.title('reward')
+    plt.xlim([-0.5, n_stps_plt+0.5])
+    plt.title(name + '  ' + str(np.mean(perf)))
+    plt.tight_layout()
+    plt.show()
+    return np.mean(perf)
+
 
 if __name__ == '__main__':
-    info()
+    info('RDM-v0')
