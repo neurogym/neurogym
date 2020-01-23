c0e6c34119912d9cfd74a67ca352da8fcb330c5a
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-23 12:11:08 +0100

    allow getting info about one particular task

--- neurogym/meta/tasks_info.py
+++ neurogym/meta/tasks_info.py
@@ -6,25 +6,27 @@ Created on Thu Jan 23 11:04:58 2020
 @author: manuel
 """
 
-"""Script to get tasks info"""
-
-
 import gym
 import neurogym as ngym
 from neurogym import all_tasks
 
 
-def info():
-
-    counter = 0
-    string = ''
-    for env_name in sorted(all_tasks.keys()):
+def info(task=None):
+    """Script to get tasks info"""
+    if task is None:
+        string = ''
+        for env_name in sorted(all_tasks.keys()):
+            string += env_name
+        print('### List of environments implemented\n\n')
+        print("* {0} tasks implemented so far.\n\n".format(len(all_tasks.keys())))
+        print('* Under development, details subject to change\n\n')
+        print(string)
+    else:
+        string = ''
         try:
-            env = gym.make(env_name)
+            env = gym.make(task)
             metadata = env.metadata
-
             string += "#### {:s}\n\n".format(type(env).__name__)
-
             paper_name = metadata.get('paper_name',
                                       None) or 'Missing paper name'
             paper_link = metadata.get('paper_link', None)
@@ -38,15 +40,15 @@ def info():
             else:
                 string += "[{:s}]({:s})\n\n".format(paper_name, paper_link)
 
-            counter += 1
+            if isinstance(env, ngym.EpochEnv):
+                timing = metadata['default_timing']
+                string += 'Default Epoch timing (ms) \n\n'
+                for key, val in timing.items():
+                    dist, args = val
+                    string += key + ' : ' + dist + ' ' + str(args) + '\n\n'
         except BaseException as e:
             print('Failure in ', env_name)
             print(e)
-
-    print('### List of environments implemented\n\n')
-    print("* {0} tasks implemented so far.\n\n".format(counter))
-    print('* Under development, details subject to change\n\n')
-    print(string)
 
 
 if __name__ == '__main__':
