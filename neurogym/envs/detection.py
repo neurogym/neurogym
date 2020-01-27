41186ee9362f208f13942860cbca271132a5e5e2
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-27 17:26:34 +0100

    detection task (need testing)

--- neurogym/envs/detection.py
+++ neurogym/envs/detection.py
@@ -12,33 +12,34 @@ import neurogym as ngym
 from neurogym.meta import tasks_info
 
 
-class SvsN(ngym.EpochEnv):
+class Detection(ngym.EpochEnv):
     metadata = {
             'description': '',
             'paper_link': '',
             'paper_name': '',
             'timing': {
                     'fixation': ('constant', 500),
-                    'noise1': ('truncated_exponential', [1500, 0, 3000]),
                     'stimulus': ('truncated_exponential', [1000, 500, 1500]),
-                    'noise2': ('truncated_exponential', [1500, 0, 3000]),
-                    'decision': ('constant', 500)}
+                    'end_of_trial': ('constant', 100)}
             }
 
-    def __init__(self, dt=100, timing=None, extra_input_param=None):
+    def __init__(self, dt=100, timing=None, noise=0.01, delay=None,
+                 stim_dur=100):
         super().__init__(dt=dt, timing=timing)
         # Possible decisions at the end of the trial
         self.choices = [0, 1]
 
         # Noise added to the observations
-        self.sigma = np.sqrt(2 * 100 * 0.01)
+        self.sigma = np.sqrt(2 * 100 * noise)
         self.sigma_dt = self.sigma / np.sqrt(self.dt)
-
+        self.delay = delay
+        self.stim_dur = int(stim_dur/self.dt)
+        assert self.stim_dur > 0, 'Stimulus duration shorter than dt'
         # Rewards
         self.R_ABORTED = -0.1  # reward given when break fixation
         self.R_CORRECT = +1.  # reward given when correct
-        self.R_FAIL = 0.  # reward given when incorrect
-        self.R_MISS = 0.  # reward given when not responding
+        self.R_FAIL = -1.  # reward given when incorrect
+        self.R_MISS = -0.5  # reward given when not responding
         # whether to abort (T) or not (F) the trial when breaking fixation:
         self.abort = False
         # action and observation spaces: [fixate, go]
@@ -64,36 +65,35 @@ class SvsN(ngym.EpochEnv):
         # Epochs
         # ---------------------------------------------------------------------
         self.add_epoch('fixation', after=0)
-        self.add_epoch('noise1', after='fixation')
-        self.add_epoch('stimulus', after='noise1')
-        self.add_epoch('noise2', after='stimulus')
-        self.add_epoch('decision', after='noise2', last_epoch=True)
+        self.add_epoch('stimulus', after='fixation')
+        self.add_epoch('end_of_trial', after='stimulus', last_epoch=True)
         # ---------------------------------------------------------------------
         # Observations
         # ---------------------------------------------------------------------
         # all observation values are 0 by default
         # FIXATION: setting fixation cue to 1 during fixation period
         self.set_ob('fixation', [1, 0])
-        # NOISE:
-        noise1 = self.view_ob('noise1')
-        noise1[:, 1:] += np.random.randn(noise1.shape[0], 1) * self.sigma_dt
-        noise2 = self.view_ob('noise2')
-        noise2[:, 1:] += np.random.randn(noise2.shape[0], 1) * self.sigma_dt
-        # STIMULUS
-        # view_ob return a pointer to observations during stimulus period:
-        stimulus = self.view_ob('stimulus')  # (shape = time x obs-dim)
+        # stimulus:
+        stim = self.view_ob('stimulus')
+        stim[:, 1:] += np.random.randn(stim.shape[0], 1) * self.sigma_dt
+        # delay
         # SET THE STIMULUS
         # adding gaussian noise to stimulus with std = self.sigma_dt
         if ground_truth == 1:
-            stimulus[:, 1:] +=\
-                0.5 + np.random.randn(stimulus.shape[0], 1) * self.sigma_dt
+            if self.delay is None:
+                delay = self.rng.randint(0, stim.shape[0]-self.stim_dur)
+            else:
+                delay = self.delay
+            stim[delay:delay + self.stim_dur, 1] += 0.5
         else:
-            stimulus[:, 1:] +=\
-                np.random.randn(stimulus.shape[0], 1) * self.sigma_dt
+            stim[:, 1:] +=\
+                np.random.randn(stim.shape[0], 1) * self.sigma_dt
+            delay = 0
+        self.delay_trial = delay*self.dt
         # ---------------------------------------------------------------------
         # Ground truth
         # ---------------------------------------------------------------------
-        self.set_groundtruth('decision', ground_truth)
+        self.set_groundtruth('end_of_trial', ground_truth)
 
     def _step(self, action):
         """
@@ -114,17 +114,32 @@ class SvsN(ngym.EpochEnv):
             if action != 0:  # if fixation break
                 new_trial = self.abort
                 reward = self.R_ABORTED
-        elif self.in_epoch('decision'):  # during decision period
+        elif self.in_epoch('stimulus'):  # during stimulus period
             if action != 0:
                 new_trial = True
-                if action == gt:  # if correct
+                if ((action == self.trial['ground_truth']) and
+                   (self.t >= self.ep_times('fixation')[1] +
+                   self.delay_trial)):
                     reward = self.R_CORRECT
                 else:  # if incorrect
                     reward = self.R_FAIL
+        elif self.in_epoch('end_of_trial'):  # during decision period
+            new_trial = True
+            if action != 0:
+                if action == self.trial['ground_truth']:  # if correct
+                    reward = self.R_CORRECT
+                else:  # if incorrect
+                    reward = self.R_FAIL
+            else:
+                if self.trial['ground_truth'] == 1:  # if correct
+                    reward = self.R_MISS
+                else:
+                    print('great!')
 
         return self.obs_now, reward, False, {'new_trial': new_trial, 'gt': gt}
 
 
 if __name__ == '__main__':
-    env = SvsN()
-    tasks_info.plot_struct(env)
+    env = Detection(noise=0, timing={'stimulus': ('constant', 200)})
+    tasks_info.plot_struct(env, num_steps_env=50,
+                           n_stps_plt=50, legend=False)  # ,def_act=1)
