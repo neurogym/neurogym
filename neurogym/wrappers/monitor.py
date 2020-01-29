8dfc07e9816890d4fd560f573b739ded8949028f
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-29 13:38:04 +0100

    allow saving extra info

--- neurogym/wrappers/monitor.py
+++ neurogym/wrappers/monitor.py
@@ -20,20 +20,24 @@ class Monitor(Wrapper):
         'folder': 'Folder where the data will be saved. (def: None)',
         'num_tr_save': '''Data will be saved every num_tr_save trials.
         (def: 100000)''',
+        'verbose': 'Whether to print information about average reward and' +
+        ' number of trials',
+        'info_keywords': '(tuple) extra information to log, from the ' +
+        'information return of environment.step'
     }
 
-    def __init__(self, env, folder=None, num_tr_save=100000):
+    def __init__(self, env, folder=None, num_tr_save=100000, verbose=False,
+                 info_keywords=()):
         Wrapper.__init__(self, env=env)
         self.env = env
         self.num_tr = 0
         # data to save
-        self.choice_mat = []
-        self.gt_mat = []
-        self.stim_mat = []
-        self.reward_mat = []
+        self.info_keywords = info_keywords
+        self.reset_data()
         self.cum_obs = 0
         self.cum_rew = 0
         self.num_tr_save = num_tr_save
+        self.verbose = verbose
         if folder is not None:
             self.folder = folder + '/'
         else:
@@ -50,27 +54,33 @@ class Monitor(Wrapper):
         self.cum_rew += rew
         if info['new_trial']:
             self.num_tr += 1
-            self.choice_mat.append(action)
-            self.stim_mat.append(self.cum_obs)
+            self.data['choice'].append(action)
+            self.data['stimulus'].append(self.cum_obs)
             self.cum_obs = 0
-            self.reward_mat.append(self.cum_rew)
+            self.data['reward'].append(self.cum_rew)
             self.cum_rew = 0
             if 'gt' in info.keys():
                 gt = np.argmax(info['gt'])
-                self.gt_mat.append(gt)
+                self.data['correct_side'].append(gt)
+            for key in self.info_keywords:
+                self.data[key].append(info[key])
 
             # save data
             if self.num_tr % self.num_tr_save == 0:
-                data = {'choice': self.choice_mat,
-                        'stimulus': self.stim_mat,
-                        'correct_side': self.gt_mat,
-                        'reward': self.reward_mat}
-
                 np.savez(self.saving_name + '_bhvr_data_' +
-                         str(self.num_tr) + '.npz', **data)
-
-                self.choice_mat = []
-                self.gt_mat = []
-                self.stim_mat = []
-                self.reward_mat = []
+                         str(self.num_tr) + '.npz', **self.data)
+                if self.verbose:
+                    print('--------------------')
+                    print('Number of steps: ', np.mean(self.num_tr))
+                    print('Average reward: ', np.mean(self.data['reward']))
+                    for key in self.info_keywords:
+                        print(key + ' : ' + str(info[key]))
+                    print('--------------------')
+                self.reset_data()
         return obs, rew, done, info
+
+    def reset_data(self):
+        data = {'choice': [], 'stimulus': [], 'correct_side': [], 'reward': []}
+        for key in self.info_keywords:
+            data[key] = []
+        self.data = data
