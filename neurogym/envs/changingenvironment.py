15eaf26ef1692ee0f88694e1d5e7544bd0fd2020
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-31 15:00:57 +0100

    minor

--- neurogym/envs/changingenvironment.py
+++ neurogym/envs/changingenvironment.py
@@ -30,13 +30,13 @@ class ChangingEnvironment(ngym.EpochEnv):
                  'context dependent']
     }
 
-    def __init__(self, dt=100, timing=None, stimEv=1., cxt_ch_prob=0.2,
+    def __init__(self, dt=100, timing=None, stimEv=1., cxt_ch_prob=0.01,
                  cxt_cue=False):
         super().__init__(dt=dt, timing=timing)
 
         # Possible contexts
         self.cxt_ch_prob = cxt_ch_prob
-        self.curr_cxt = 0
+        self.curr_cxt = self.rng.choice([0, 1])
         self.cxt_cue = cxt_cue
 
         # Possible decisions at the end of the trial
@@ -57,7 +57,8 @@ class ChangingEnvironment(ngym.EpochEnv):
         # action and observation spaces: [fixate, got left, got right]
         self.action_space = spaces.Discrete(5)
         # observation space: [fixation cue, left stim, right stim]
-        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(3,),
+        self.observation_space = spaces.Box(-np.inf, np.inf,
+                                            shape=(3+1*cxt_cue,),
                                             dtype=np.float32)
 
     def new_trial(self, **kwargs):
@@ -98,16 +99,32 @@ class ChangingEnvironment(ngym.EpochEnv):
         # ---------------------------------------------------------------------
         # all observation values are 0 by default
         # FIXATION: setting fixation cue to 1 during fixation period
-        self.set_ob('fixation', [1, 0, 0])
-        # STIMULUS
-        # view_ob return a pointer to observations during stimulus period:
-        stimulus = self.view_ob('stimulus')  # (shape = time x obs-dim)
-        stimulus[:, 1:] = (1 - coh / 100) / 2
-        # coh for correct side
-        stimulus[:, side] = (1 + coh / 100) / 2
-        # adding gaussian noise to stimulus with std = self.sigma_dt
-        stimulus[:, 1:] +=\
-            np.random.randn(stimulus.shape[0], 2) * self.sigma_dt
+        if not self.cxt_cue:
+            self.set_ob('fixation', [1, 0, 0])
+            # STIMULUS
+            # view_ob return a pointer to observations during stimulus period:
+            stimulus = self.view_ob('stimulus')  # (shape = time x obs-dim)
+            stimulus[:, 1:] = (1 - coh / 100) / 2
+            # coh for correct side
+            stimulus[:, side] = (1 + coh / 100) / 2
+            # adding gaussian noise to stimulus with std = self.sigma_dt
+            stimulus[:, 1:] +=\
+                np.random.randn(stimulus.shape[0], 2) * self.sigma_dt
+        else:
+            self.set_ob('fixation', [self.curr_cxt, 0, 0, 0])
+            self.set_ob('stimulus', [self.curr_cxt, 0, 0, 0])
+            self.set_ob('decision', [self.curr_cxt, 0, 0, 0])
+            self.set_ob('fixation', [0, 1, 0, 0])
+            # STIMULUS
+            # view_ob return a pointer to observations during stimulus period:
+            stimulus = self.view_ob('stimulus')  # (shape = time x obs-dim)
+            stimulus[:, 2:] = (1 - coh / 100) / 2
+            # coh for correct side
+            stimulus[:, side] = (1 + coh / 100) / 2
+            # adding gaussian noise to stimulus with std = self.sigma_dt
+            stimulus[:, 2:] +=\
+                np.random.randn(stimulus.shape[0], 2) * self.sigma_dt
+
         # ---------------------------------------------------------------------
         # Ground truth
         # ---------------------------------------------------------------------
@@ -141,13 +158,9 @@ class ChangingEnvironment(ngym.EpochEnv):
                 else:  # if incorrect
                     reward = self.R_FAIL
 
-        if self.cxt_cue:
-            cue = np.array([self.curr_cxt])
-            obs = np.concatenate((cue, obs), axis=0)
-
         return obs, reward, False, {'new_trial': new_trial, 'gt': gt}
 
 
 if __name__ == '__main__':
-    env = CE(cxt_cue=True)
+    env = ChangingEnvironment(cxt_ch_prob=0.05, stimEv=100, cxt_cue=False)
     info.plot_struct(env)
