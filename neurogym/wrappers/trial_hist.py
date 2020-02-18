34b1718bf103e2fe60a92b8a62d8ad4b740c88ca
manuelmolano <manuelmolanomazon@gmail.com>
2020-02-18 16:42:10 +0100

    update trial_hist to allow more than 2 choices

--- neurogym/wrappers/trial_hist.py
+++ neurogym/wrappers/trial_hist.py
@@ -18,29 +18,35 @@ class TrialHistory(ngym.TrialWrapper):
         'on perceptual decisions'
     }
 
-    def __init__(self, env, rep_prob=(.2, .8), block_dur=200,
+    def __init__(self, env, probs=None, block_dur=200,
                  blk_ch_prob=None):
         """
         Change ground truth probability based on previous outcome.
-        rep_prob: Specifies probabilities of repeating for each block.
-        (def: (.2, .8), tuple)
-        block_dur: Number of trials per block. (def: 200 (int)
+        probs: matrix of probabilities of the current choice conditioned
+        on the previous for each block. (def: None, np.array)
+        block_dur: Number of trials per block. (def: 200 (int))
         blk_ch_prob: If not None, specifies the probability of changing block
         (randomly). (def: None, float)
         """
         super().__init__(env)
+        try:
+            self.choices = self.task.choices
+        except AttributeError:
+            raise AttributeError('''SideBias requires task
+                                 to have attribute choices''')
+        assert isinstance(self.task, ngym.TrialEnv), 'Task has to be TrialEnv'
+        assert probs is not None, 'Please provide choices probabilities'
+        assert probs.shape[1] == len(self.choices),\
+            'The number of choices {:d}'.format(probs.shape[1]) +\
+            ' inferred from prob mismatchs {:d}'.format(len(self.choices)) +\
+            ' inferred from choices'
 
-        self.rep_prob = rep_prob
-        # keeps track of the repeating prob of the current block
-        self.curr_block = self.task.rng.choice([0, 1])
-        # duration of block (in number oif trials)
+        self.n_block = self.choice_prob.shape[0]
+        self.curr_block = self.task.rng.choice(range(self.n_block))
+        self.probs = probs
         self.block_dur = block_dur
         self.prev_trial = -1
         self.blk_ch_prob = blk_ch_prob
-
-        if len(self.task.choices) != 2:
-            raise ValueError('Currently,' +
-                             ' this wrapper only works this 2AFC envs.')
 
     def new_trial(self, **kwargs):
         # ---------------------------------------------------------------------
@@ -54,14 +60,8 @@ class TrialHistory(ngym.TrialWrapper):
             if self.task.rng.random() < self.blk_ch_prob:
                 self.curr_block = (self.curr_block + 1) % len(self.rep_prob)
 
-        rep_prob = self.rep_prob[self.curr_block]
-
-        if self.prev_trial == self.task.choices[0]:
-            probs = (rep_prob, 1-rep_prob)
-        else:
-            probs = (1-rep_prob, rep_prob)
-
-        ground_truth = self.task.rng.choice(self.task.choices, p=probs)
+        ground_truth = self.task.rng.choice(self.task.choices,
+                                            p=self.probs[self.prev_trial, :])
         self.prev_trial = ground_truth
         kwargs.update({'ground_truth': ground_truth})
         self.env.new_trial(**kwargs)
