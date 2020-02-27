5e9ca9ab398b77a7323f195026c2f00b33165ee7
gryang <gyyang.neuro@gmail.com>
2020-02-27 14:26:44 -0500

    adding an example collection

--- neurogym/collections/example.py
+++ neurogym/collections/example.py
@@ -4,10 +4,43 @@ import gym
 from neurogym.wrappers import PassAction
 from neurogym.tests.test_envs import test_run
 
-env_name = 'PerceptualDecisionMaking-v0'
-env_kwargs = {'dt': 20, 'stim_scale': 0.5}
-env = gym.make(env_name, **env_kwargs)
-env = PassAction(env)
 
-test_run(env)
+def main():
+    envs = list()
+    dt = 100
 
+    env_kwargs = {'dt': dt}
+    env = gym.make('PerceptualDecisionMaking-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('ContextDecisionMaking-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('MultiSensoryIntegration-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('DelayedComparison-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('DelayedMatchSample-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('DelayedMatchCategory-v0', **env_kwargs)
+    envs.append(env)
+
+    env_kwargs = {'dt': dt}
+    env = gym.make('AntiReach-v0', **env_kwargs)
+    envs.append(env)
+    
+    return envs
+
+
+if __name__ == '__main__':
+    envs = main()
+    for env in envs:
+        test_run(env)
