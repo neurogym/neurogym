e4c8d9d888b5cd2778a40c140736043f4347e567
gryang <gyyang.neuro@gmail.com>
2020-02-27 14:02:09 -0500

    adding multisensory task

--- neurogym/envs/__init__.py
+++ neurogym/envs/__init__.py
@@ -19,6 +19,7 @@ ALL_ENVS = {'ContextDecisionMaking-v0': 'neurogym.envs.contextdecisionmaking:Con
             'DawTwoStep-v0': 'neurogym.envs.dawtwostep:DawTwoStep',
             'MatchingPenny-v0': 'neurogym.envs.matchingpenny:MatchingPenny',
             'MotorTiming-v0': 'neurogym.envs.readysetgo:MotorTiming',
+            'MultiSensoryIntegration-v0': 'neurogym.envs.multisensory:MultiSensoryIntegration',
             'Bandit-v0': 'neurogym.envs.bandit:Bandit',
             'PerceptualDecisionMakingDelayResponse-v0':
                 'neurogym.envs.perceptualdecisionmaking:PerceptualDecisionMakingDelayResponse',
