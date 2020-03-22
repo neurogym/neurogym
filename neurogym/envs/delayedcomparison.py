d8479a97b31bea00b4d2d4c313dad7a26699df70
gyyang <gyyang.neuro@gmail.com>
2020-03-22 12:12:03 -0400

    minor

--- neurogym/envs/delayedcomparison.py
+++ neurogym/envs/delayedcomparison.py
@@ -24,7 +24,6 @@ class DelayedComparison(ngym.PeriodEnv):
         super().__init__(dt=dt)
 
         # trial conditions
-        self.choices = [1, 2]
         self.fpairs = [(18, 10), (22, 14), (26, 18), (30, 22), (34, 26)]
 
         # Input noise
@@ -54,16 +53,18 @@ class DelayedComparison(ngym.PeriodEnv):
 
         # action and observation space
         self.action_space = spaces.Discrete(3)
+        self.act_dict = {'fixation': 0, 'choice': [1, 2]}
         self.observation_space = spaces.Box(-np.inf, np.inf, shape=(2,),
                                             dtype=np.float32)
         self.ob_dict = {'fixation': 0, 'stimulus': 1}
 
     def new_trial(self, **kwargs):
         self.trial = {
-            'ground_truth': self.rng.choice(self.choices),
+            'ground_truth': self.rng.choice(self.act_dict['choice']),
             'fpair': self.fpairs[self.rng.choice(len(self.fpairs))]
         }
         self.trial.update(kwargs)
+
         f1, f2 = self.trial['fpair']
         if self.trial['ground_truth'] == 2:
             f1, f2 = f2, f1
@@ -77,8 +78,7 @@ class DelayedComparison(ngym.PeriodEnv):
         self.add_ob(self.scale_p(f1), 'f1', where='stimulus')
         self.add_ob(self.scale_p(f2), 'f2', where='stimulus')
         self.set_ob(0, 'decision')
-        self.add_randn(0, self.sigma_dt, 'f1')
-        self.add_randn(0, self.sigma_dt, 'f2')
+        self.add_randn(0, self.sigma_dt, ['f1', 'f2'])
 
         self.set_groundtruth(self.trial['ground_truth'], 'decision')
 
@@ -97,7 +97,7 @@ class DelayedComparison(ngym.PeriodEnv):
         # ---------------------------------------------------------------------
         new_trial = False
         gt = self.gt_now
-        obs = self.ob_now
+        ob = self.ob_now
         # rewards
         reward = 0
         if self.in_period('fixation'):
@@ -113,7 +113,7 @@ class DelayedComparison(ngym.PeriodEnv):
                 else:
                     reward = self.rewards['fail']
 
-        return obs, reward, False, {'new_trial': new_trial, 'gt': gt}
+        return ob, reward, False, {'new_trial': new_trial, 'gt': gt}
 
 
 if __name__ == '__main__':
