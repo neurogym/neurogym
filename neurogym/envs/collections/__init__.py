579878b5c6b39aab464bce929aeef983c6b55544
gryang <gyyang.neuro@gmail.com>
2020-06-11 17:07:40 -0400

    more explicit return of collection

--- neurogym/envs/collections/__init__.py
+++ neurogym/envs/collections/__init__.py
@@ -9,9 +9,18 @@ import importlib
 def _collection_from_file(fname):
     """Return list of envs from file."""
     lib = 'neurogym.envs.collections.' + fname
-    module = importlib.import_module(lib)
-    envs = [name for name, val in getmembers(module) if isfunction(val)]
-    envs = [fname + '.' + env + '-v0' for env in envs if env[0] != '_']
+    if fname == 'yang19':
+        envs = ['go', 'rtgo', 'dlygo', 'anti', 'rtanti', 'dlyanti',
+                'dm1', 'dm2', 'ctxdm1', 'ctxdm2', 'multidm',
+                'dlydm1', 'dlydm2', 'ctxdlydm1', 'ctxdlydm2', 'multidlydm',
+                'dms', 'dnms', 'dmc', 'dnmc']
+    else:
+        module = importlib.import_module(lib)
+        envs = [name for name, val in getmembers(module) if isfunction(val)]
+        envs = sorted(envs)
+        envs = [env for env in envs if env[0] != '_']
+
+    envs = [fname + '.' + env + '-v0' for env in envs]
     return envs
 
 
