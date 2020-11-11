d7a06be0167397ca1c3d08f35f38a399d0e821d7
mitchellostrow <35669245+mitchellostrow@users.noreply.github.com>
2020-11-11 11:55:16 -0500

    Make pass_action one-hot encoded

--- neurogym/wrappers/pass_action.py
+++ neurogym/wrappers/pass_action.py
@@ -5,11 +5,10 @@ import numpy as np
 from gym import Wrapper
 from gym import spaces
 
-
 class PassAction(Wrapper):
     """Modifies observation by adding the previous action."""
     metadata = {
-        'description': 'Modifies observation by adding the previous action.',
+        'description': 'Modifies observation by adding the previous action as one-hot encoded vector',
         'paper_link': None,
         'paper_name': None,
     }
@@ -17,17 +16,21 @@ class PassAction(Wrapper):
     def __init__(self, env):
         super().__init__(env)
         self.env = env
-        # TODO: This is not adding one-hot
         env_oss = env.observation_space.shape[0]
+        self.num_act = env.action_space.n
         self.observation_space = spaces.Box(-np.inf, np.inf,
-                                            shape=(env_oss+1,),
+                                            shape=(env_oss+self.num_act,),
                                             dtype=np.float32)
+  
     def reset(self, step_fn=None):
         if step_fn is None:
             step_fn = self.step
         return self.env.reset(step_fn=step_fn)
 
     def step(self, action):
+        actions = np.zeros(self.num_act)
+        actions[action] = 1
         obs, reward, done, info = self.env.step(action)
-        obs = np.concatenate((obs, np.array([action])))
+        obs = np.concatenate((obs, actions))
         return obs, reward, done, info
+
