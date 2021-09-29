0d76e6aec0400436564ce734e9093a2e403d48e7
manuelmolano <manuelmolanomazon@gmail.com>
2021-09-29 19:05:25 +0200

    final code to produce tables in latex, added list of refs in overleaf

--- docs/latex_table.py
+++ docs/latex_table.py
@@ -13,7 +13,14 @@ import neurogym as ngym
 from neurogym.utils.info import info, info_wrapper
 from neurogym.envs.registration import ALL_ENVS
 FOLDER = '/home/molano/Desktop/'
-
+ref_list = ['munoz2004look', 'wang2018prefrontal', 'mante2013context',
+            'daw2011model', 'barak2010neuronal', 'freedman2006experience',
+            'miller1996neural', 'zhang2019active', 'rose2016reactivation',
+            'padoa2006neurons', 'sarafyazd2019hierarchical', 'genovesio2009feature',
+            'wang2018flexible', 'egger2019internal', 'britten1992analysis',
+            'inagaki2019discrete', 'kiani2009representation',
+            'yang2007probabilistic', 'scott2015sources',
+            'georgopoulos1986neuronal', 'tadin2003perceptual']
 SOURCE_ROOT = 'https://github.com/gyyang/neurogym/blob/master/'
 
 
@@ -36,7 +43,7 @@ def write_doc(write_type):
         raise ValueError
 
     string = '\\begin{center}\n\\begin{tabular}' +\
-        '{ | m{7cm}| m{7cm} | m{1cm} |}\n\\hline\n'
+        '{ | m{7cm}| m{9cm} |}\n\\hline\n'
     counter = 0
     for name in all_items:
         try:
@@ -44,20 +51,18 @@ def write_doc(write_type):
             info_string = info_fn(name)
             name = name.replace('-v0', '')
             info_string = info_string.replace('\n', ' ')
-            string += name+' &'
+            info_string = info_string.replace('Doc: ', '')
+            string += name+'~\\cite{bibreference} &'
             start = info_string.find(name)+len(name)
             start = 0 if start == -1 else start
             end = info_string.find('Args:')
             end = info_string.find('Reference paper') if end == -1 else end
-            string += info_string[start:end]+' &'
-            start = info_string.find('Reference paper')+len('Reference paper')
-            end = info_string.find('Reward struct')
             string += info_string[start:end]+'\\\\\n\\hline\n'
             counter += 1
             if (counter+1) % 5 == 0:
                 string += '\\end{tabular}\n\\end{center}\n'
                 string += '\\begin{center}\n\\begin{tabular}' +\
-                    '{ | m{7cm}| m{7cm} | m{1cm} |}\n\\hline\n'
+                    '{ | m{7cm}| m{9cm} |}\n\\hline\n'
 
         except BaseException as e:
             print('Failure in ', name)
@@ -70,7 +75,7 @@ def write_doc(write_type):
 
 
 def main():
-    write_doc('tasks')
+    # write_doc('tasks')
     write_doc('wrappers')
 
 
