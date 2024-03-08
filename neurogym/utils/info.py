469daa95f4d5b412669702f7a4823567c5743402
nacloos <cloosnathan@gmail.com>
2024-03-08 12:38:52 -0500

    Remove try except block in env info (was hiding errors)

--- neurogym/utils/info.py
+++ neurogym/utils/info.py
@@ -5,6 +5,7 @@
 import inspect
 
 import gym
+import neurogym as ngym
 from neurogym.core import env_string, METADATA_DEF_KEYS
 from neurogym.envs.registration import all_envs, ALL_ENVS
 from neurogym.wrappers import ALL_WRAPPERS
@@ -23,63 +24,60 @@ def all_wrappers():
 def info(env=None, show_code=False):
     """Script to get envs info"""
     string = ''
-    try:
-        env_name = env
-        env = gym.make(env)
-        string = env_string(env)
-        # show source code
-        if show_code:
-            string += '''\n#### Source code #### \n\n'''
-            env_ref = ALL_ENVS[env_name]
-            from_, class_ = env_ref.split(':')
-            imported = getattr(__import__(from_, fromlist=[class_]), class_)
-            lines = inspect.getsource(imported)
-            string += lines + '\n\n'
-        print(string)
-    except BaseException as e:
-        print('Failure in ', type(env).__name__)
-        print(e)
+    env_name = env
+    env = ngym.make(env)
+    # remove extra wrappers (make can add a OrderEnforcer wrapper)
+    env = env.unwrapped
+    string = env_string(env)
+    # show source code
+    if show_code:
+        string += '''\n#### Source code #### \n\n'''
+        env_ref = ALL_ENVS[env_name]
+        from_, class_ = env_ref.split(':')
+        imported = getattr(__import__(from_, fromlist=[class_]), class_)
+        lines = inspect.getsource(imported)
+        string += lines + '\n\n'
     return string
 
 
 def info_wrapper(wrapper=None, show_code=False):
     """Script to get wrappers info"""
     string = ''
-    try:
-        wrapp_ref = ALL_WRAPPERS[wrapper]
-        from_, class_ = wrapp_ref.split(':')
-        imported = getattr(__import__(from_, fromlist=[class_]), class_)
-        metadata = imported.metadata
-        string += "### {:s}\n\n".format(wrapper)
-        paper_name = metadata.get('paper_name',
-                                  None)
-        paper_link = metadata.get('paper_link', None)
-        wrapper_description = metadata.get('description',
-                                           None) or 'Missing description'
-        string += "Logic: {:s}\n\n".format(wrapper_description)
-        if paper_name is not None:
-            string += "Reference paper \n\n"
-            if paper_link is None:
-                string += "{:s}\n\n".format(paper_name)
-            else:
-                string += "[{:s}]({:s})\n\n".format(paper_name,
-                                                    paper_link)
-        # add extra info
-        other_info = list(set(metadata.keys()) - set(METADATA_DEF_KEYS))
-        if len(other_info) > 0:
-            string += "Input parameters: \n\n"
-            for key in other_info:
-                string += key + ' : ' + str(metadata[key]) + '\n\n'
 
-        # show source code
-        if show_code:
-            string += '''\n#### Source code #### \n\n'''
-            lines = inspect.getsource(imported)
-            string += lines + '\n\n'
-    except BaseException as e:
-        print('Failure in ', wrapper)
-        print(e)
-    print(string)
+    wrapp_ref = ALL_WRAPPERS[wrapper]
+    from_, class_ = wrapp_ref.split(':')
+    imported = getattr(__import__(from_, fromlist=[class_]), class_)
+    metadata = imported.metadata
+
+    if not isinstance(metadata, dict):
+        # same wrappers don't specify metadata dict but still have a metadata property inherited from gym.Wrapper (which causes an error when trying to access keys)
+        metadata = {}
+
+    string += "### {:s}\n\n".format(wrapper)
+    paper_name = metadata.get('paper_name', None)
+    paper_link = metadata.get('paper_link', None)
+    wrapper_description = metadata.get('description', None) or 'Missing description'
+    string += "Logic: {:s}\n\n".format(wrapper_description)
+    if paper_name is not None:
+        string += "Reference paper \n\n"
+        if paper_link is None:
+            string += "{:s}\n\n".format(paper_name)
+        else:
+            string += "[{:s}]({:s})\n\n".format(paper_name,
+                                                paper_link)
+    # add extra info
+    other_info = list(set(metadata.keys()) - set(METADATA_DEF_KEYS))
+    if len(other_info) > 0:
+        string += "Input parameters: \n\n"
+        for key in other_info:
+            string += key + ' : ' + str(metadata[key]) + '\n\n'
+
+    # show source code
+    if show_code:
+        string += '''\n#### Source code #### \n\n'''
+        lines = inspect.getsource(imported)
+        string += lines + '\n\n'
+
     return string
 
 
@@ -89,7 +87,7 @@ def all_tags(verbose=0):
     tags = []
     for env_name in sorted(envs):
         try:
-            env = gym.make(env_name)
+            env = ngym.make(env_name)
             metadata = env.metadata
             tags += metadata.get('tags', [])
         except BaseException as e:
