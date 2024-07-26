234826aecc264f4d18e543b6fb852fc092636ed4
DaniBodor <d.bodor@esciencecenter.nl>
2024-07-26 21:57:10 +0200

    test_envs

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -10,28 +10,22 @@ from neurogym.utils.data import Dataset
 try:
     import psychopy  # noqa: F401
 
-    _have_psychopy = True
+    _have_psychopy = True  # FIXME should psychopy be always tested, to ensure CI doesn't fail?
 except ImportError:
     _have_psychopy = False
 
 ENVS = ngym.all_envs(psychopy=_have_psychopy, contrib=True, collections=True)
 # Envs without psychopy, TODO: check if contrib or collections include psychopy
 ENVS_NOPSYCHOPY = ngym.all_envs(psychopy=False, contrib=True, collections=True)
+SEED = 0
 
 
-def make_env(env, **kwargs):
-    # use ngym.make and not gym.make to disable env_checker
-    return ngym.make(env, **kwargs)
-
-
-def test_run(env=None, num_steps=100, verbose=False, **kwargs):
+def _test_run(env, num_steps=100, verbose=False):
     """Test if one environment can at least be run."""
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        if env is None:
-            env = ngym.all_envs()[0]
         if isinstance(env, str):
-            env = make_env(env, **kwargs)
+            env = ngym.make(env)
         elif not isinstance(env, gym.Env):
             msg = f"{type(env)=} must be a string or a gym.Env"
             raise TypeError(msg)
@@ -60,18 +54,17 @@ def test_run_all(verbose_success=False):
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        for env_name in sorted(ENVS):
+        assert ngym.all_envs()[0] in ENVS
+        for env_name in ENVS:
             print(env_name)
-            test_run(env_name, verbose=verbose_success)
+            _test_run(env_name, verbose=verbose_success)
 
 
-def test_dataset(env=None):
+def _test_dataset(env):
     """Main function for testing if an environment is healthy."""
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        if env is None:
-            env = ngym.all_envs()[0]
 
         print("Testing Environment:", env)
         kwargs = {"dt": 20}
@@ -91,10 +84,10 @@ def test_dataset_all():
         success_count = 0
         total_count = len(ngym.all_envs())
         supervised_count = len(ngym.all_envs(tag="supervised"))
-        for env_name in sorted(ngym.all_envs()):
+        for env_name in ngym.all_envs():
             print(f"Running env: {env_name}")
-            try:
-                test_dataset(env_name)
+            try:  # FIXME, tests are not actually performed here, as any error is caught away
+                _test_dataset(env_name)
                 print("Success")
                 success_count += 1
             except BaseException as e:  # noqa: BLE001 # FIXME: unclear which error is expected here.
@@ -110,27 +103,27 @@ def test_print_all():
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        for env_name in sorted(ENVS):
+        for env_name in ENVS:
             print()
             print(f"Test printing env: {env_name}")
-            env = make_env(env_name)
+            env = ngym.make(env_name)
             print(env)
 
 
-def test_trialenv(env=None, **kwargs):
+def _test_trialenv(env):
     """Test if a TrialEnv is behaving correctly."""
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        if env is None:
-            env = ngym.all_envs()[0]
         if isinstance(env, str):
-            env = make_env(env, **kwargs)
+            env = ngym.make(env)
         elif not isinstance(env, gym.Env):
             msg = f"{type(env)=} must be a string or a gym.Env"
             raise TypeError(msg)
         trial = env.new_trial()
-        assert trial is not None, f"TrialEnv should return trial info dict {env}"
+        assert (
+            trial is not None
+        ), f"TrialEnv should return trial info dict {env}"  # FIXME: should we assert isinstance(trial, dict) instead?
 
 
 def test_trialenv_all():
@@ -138,14 +131,14 @@ def test_trialenv_all():
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        for env_name in sorted(ENVS):
-            env = make_env(env_name)
-            if not isinstance(env, ngym.TrialEnv):
+        for env_name in ENVS:
+            env = ngym.make(env_name)
+            if not isinstance(env, ngym.TrialEnv):  # FIXME: probably these should be flagged rather than skipped
                 continue
-            test_trialenv(env)
+            _test_trialenv(env)
 
 
-def test_seeding(env=None, seed=0):
+def _test_seeding(env):
     """Test if environments are replicable."""
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
@@ -155,12 +148,12 @@ def test_seeding(env=None, seed=0):
 
         if isinstance(env, str):
             kwargs = {"dt": 20}
-            env = make_env(env, **kwargs)
+            env = ngym.make(env, **kwargs)
         elif not isinstance(env, gym.Env):
             msg = f"{type(env)=} must be a string or a gym.Env"
             raise TypeError(msg)
 
-        env.seed(seed)
+        env.seed(SEED)
         env.reset()
         ob_mat = []
         rew_mat = []
@@ -186,10 +179,11 @@ def test_seeding(env=None, seed=0):
 # TODO: there is one env for which it sometimes raises an error
 def test_seeding_all():
     """Test if all environments are replicable."""
+    assert ngym.all_envs()[0] in ENVS_NOPSYCHOPY
     for env_name in sorted(ENVS_NOPSYCHOPY):
         print(f"Running env: {env_name}")
-        obs1, rews1, acts1 = test_seeding(env_name, seed=0)
-        obs2, rews2, acts2 = test_seeding(env_name, seed=0)
+        obs1, rews1, acts1 = _test_seeding(env_name)
+        obs2, rews2, acts2 = _test_seeding(env_name)
         assert (obs1 == obs2).all(), "obs are not identical"
         assert (rews1 == rews2).all(), "rewards are not identical"
         assert (acts1 == acts2).all(), "actions are not identical"
@@ -199,12 +193,12 @@ def test_plot_envs():
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*get variables from other wrappers is deprecated*")
         warnings.filterwarnings("ignore", message=".*The environment creator metadata doesn't include `render_modes`*")
-        for env_name in sorted(ENVS):
+        for env_name in ENVS:
             if env_name == "Null-v0":
                 continue
-            env = make_env(env_name, dt=20)
+            env = ngym.make(env_name, dt=20)
             action = np.zeros_like(env.action_space.sample())
-            try:
+            try:  # FIXME: no actual test is run, as errors are caught
                 ngym.utils.plot_env(env, num_trials=2, def_act=action)
             except Exception as e:  # noqa: BLE001 # FIXME: unclear which error is expected here.
                 print(f"Error in plotting env: {env_name}, {e}")
