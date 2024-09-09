60324c112a76767cf6f5d057cb2c9dc3a5d81a2d
gcroci2 <crocioni.giulia@gmail.com>
2024-09-09 13:44:19 +0200

    remove decision time and set gt to coincide with the stimuli when present

--- neurogym/envs/annubes.py
+++ neurogym/envs/annubes.py
@@ -10,7 +10,6 @@ class AnnubesEnv(TrialEnv):
         session=None,
         stim_intensities=None,
         stim_time=1000,
-        decision_time=100,
         catch_prob=0.5,
         fix_intensity=0,
         fix_time=500,
@@ -32,7 +31,6 @@ class AnnubesEnv(TrialEnv):
         self.session = session
         self.stim_intensities = stim_intensities
         self.stim_time = stim_time
-        self.decision_time = decision_time
         self.catch_prob = catch_prob
         self.fix_intensity = fix_intensity
         self.fix_time = fix_time
@@ -50,10 +48,16 @@ class AnnubesEnv(TrialEnv):
             random_seed = rng.integers(2**32)
         self._rng = np.random.default_rng(random_seed)
         self._random_seed = random_seed
-        self.timing = {"fixation": self.fix_time, "stimulus": self.stim_time, "decision": self.decision_time}
+        self.timing = {"fixation": self.fix_time, "stimulus": self.stim_time}
         # Set the name of each input dimension
         obs_space_name = {"fixation": 0, "start": 1, "v": 2, "a": 3}
-        self.observation_space = ngym.spaces.Box(low=0.0, high=1.0, shape=(len(obs_space_name),), name=obs_space_name)
+        self.observation_space = ngym.spaces.Box(
+            low=0.0,
+            high=1.0,
+            shape=(len(obs_space_name),),
+            dtype=np.float32,
+            name=obs_space_name,
+        )
         # Set the name of each action value
         self.action_space = ngym.spaces.Discrete(
             self.n_outputs,
@@ -63,37 +67,41 @@ class AnnubesEnv(TrialEnv):
         self.rewards = {"abort": -0.1, "correct": +1.0, "fail": 0.0}
 
     def _new_trial(self):
+        # Reset trial-related variables
+        self.trial = {}
+
         # Setting time periods and their order for this trial
-        self.add_period(["fixation", "stimulus", "decision"])
+        self.add_period(["fixation", "stimulus"])
 
         # Adding fixation and start signal values
         self.add_ob(self.fix_intensity, "fixation", where="fixation")
         self.add_ob(1, "stimulus", where="start")
 
         # Catch trial decision
-        catch = self.rng.choice([0, 1], p=[self.catch_prob, 1 - self.catch_prob])
+        catch = self._rng.choice([0, 1], p=[self.catch_prob, 1 - self.catch_prob])
+        stim_type = None
+        stim_value = None
         if not catch:
-            stim_type = self.rng.choice(list(self.session.keys()), p=list(self.session.values()))
+            stim_type = self._rng.choice(list(self.session.keys()), p=list(self.session.values()))
             stim_value = self._rng.choice(self.stim_intensities, 1)
-
-        for mod in self.session:
-            stim = stim_value if not catch and stim_type == mod else 0
-            self.add_ob(stim, "stimulus", where=mod)
-
-        if not catch:
-            self.add_randn(0, self.noise_factor, "stimulus", where=stim_type)
-
-        # Set ground_truth
-        groundtruth = 1 if not catch else 0
-        self.set_groundtruth(groundtruth, period="decision", where="choice")
+            for mod in self.session:
+                if stim_type == mod:
+                    self.add_ob(stim_value, "stimulus", where=mod)
+                    self.add_randn(0, self.noise_factor, "stimulus", where=mod)
+                self.set_groundtruth(0, period="fixation")
+                self.set_groundtruth(1, period="stimulus")
+        else:
+            self.set_groundtruth(0, period="fixation")
+            self.set_groundtruth(0, period="stimulus")
 
         # Trial information
-        # TODO: align groundtruth to annubes
-        trial = {
-            "ground_truth": groundtruth,
+        self.trial = {
+            "catch": catch,
+            "stim_type": stim_type,
+            "stim_value": stim_value,
         }
 
-        return trial
+        return self.trial
 
     def _step(self, action):
         new_trial = False
@@ -101,16 +109,28 @@ class AnnubesEnv(TrialEnv):
         truncated = False
         reward = 0
         gt = self.gt_now
+
         if self.in_period("fixation"):
             if action != 0:
-                new_trial = False
                 reward += self.rewards["abort"]
-        elif self.in_period("decision") and action != 0:
-            new_trial = True
+        elif self.in_period("stimulus"):
             if action == gt:
                 reward += self.rewards["correct"]
                 self.performance = 1
             else:
                 reward += self.rewards["fail"]
 
-        return self.ob_now, reward, terminated, truncated, {"new_trial": new_trial, "gt": gt}
+            # End trial when stimulus period is over
+            # self.t represents the current time step within a trial
+            # esch step is self.dt ms
+            # self.tmax is the maximum number of time steps within a trial
+            # see self.add_period in TrialEnv for more details
+            if self.t >= self.tmax - self.dt:
+                new_trial = True
+
+        info = {"new_trial": new_trial, "gt": gt}
+        if new_trial:
+            info["trial"] = self.trial
+            self.trial = {}
+
+        return self.ob_now, reward, terminated, truncated, info
