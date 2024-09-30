75956afdd7512d133f33fe75d3980acd5ebe20c2
gcroci2 <crocioni.giulia@gmail.com>
2024-09-30 10:50:44 +0200

    fix mypy error

--- neurogym/envs/annubes.py
+++ neurogym/envs/annubes.py
@@ -103,7 +103,7 @@ class AnnubesEnv(TrialEnv):
             name={"fixation": self.output_behavior[0], "choice": self.output_behavior[1:]},
         )
 
-    def _new_trial(self, **kwargs: Any) -> dict[str, Any]:  # type: ignore[override]
+    def _new_trial(self, **kwargs: Any) -> dict[str, bool | Any | None]:  # type: ignore[override]
         """Internal method to generate a new trial.
 
         Returns:
@@ -133,9 +133,7 @@ class AnnubesEnv(TrialEnv):
             self.set_groundtruth(0, period="fixation")
             self.set_groundtruth(0, period="stimulus")
 
-        self.trial = {"catch": catch, "stim_type": stim_type, "stim_value": stim_value}
-
-        return self.trial
+        return {"catch": catch, "stim_type": stim_type, "stim_value": stim_value}
 
     def _step(self, action: int) -> tuple:  # type: ignore[override]
         """Internal method to compute the environment's response to the agent's action.
