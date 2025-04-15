bd39c9c04eae201755312181f6e82644ff95b223
DaniBodor <d.bodor@esciencecenter.nl>
2025-04-15 14:23:21 +0200

    refactor: allow Gymnasium to handle invalid envs
    Gymnasium already implements different errors dependent on whether the
    namespace, environment, or version does not exist. It also already
    gives a suggestion for what you may have meant if the env is invalid.

--- neurogym/envs/registration.py
+++ neurogym/envs/registration.py
@@ -206,44 +206,10 @@ def all_tags():
     ]
 
 
-def _distance(s0, s1):
-    # Copyright (c) 2018 luozhouyang
-    if s0 is None:
-        msg = "Argument s0 is NoneType."
-        raise TypeError(msg)
-    if s1 is None:
-        msg = "Argument s1 is NoneType."
-        raise TypeError(msg)
-    if s0 == s1:
-        return 0.0
-    if len(s0) == 0:
-        return len(s1)
-    if len(s1) == 0:
-        return len(s0)
-
-    v0 = [0] * (len(s1) + 1)
-    v1 = [0] * (len(s1) + 1)
-
-    for i in range(len(v0)):
-        v0[i] = i
-
-    for i in range(len(s0)):
-        v1[0] = i + 1
-        for j in range(len(s1)):
-            cost = 1
-            if s0[i] == s1[j]:
-                cost = 0
-            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
-        v0, v1 = v1, v0
-
-    return v0[len(s1)]
-
-
 def make(id_: str, **kwargs) -> gym.Env:
     """Creates an environment previously registered with :meth:`ngym.register`.
 
-    This function attempts to create an environment using the given `id_`. If the environment
-    is not registered, it raises an error and suggests the closest matching environment IDs.
+    This function calls the Gymnasium `make` function with the `disable_env_checker` argument set to True.
 
     Args:
         id_: A string representing the environment ID.
@@ -251,30 +217,9 @@ def make(id_: str, **kwargs) -> gym.Env:
 
     Returns:
         An instance of the environment.
-
-    Raises:
-        gym.error.UnregisteredEnv: If the `id_` doesn't exist in the Neurogym's registry. The error
-            message will include suggestions for the closest matching environment IDs.
     """
-    try:
-        # built in env_checker raises warnings or errors when ob not in observation_space
-        return gym.make(id_, disable_env_checker=True, **kwargs)
-
-    except gym.error.UnregisteredEnv as e:
-        # backward compatibility with old versions of gym
-        if hasattr(gym.envs.registry, "all"):
-            all_ids = [env.id for env in gym.envs.registry.all()]
-        else:
-            all_ids = [env.id for env in gym.envs.registry.values()]
-
-        dists = [_distance(id_, env_id) for env_id in all_ids]
-        # Python argsort
-        sort_inds = sorted(range(len(dists)), key=dists.__getitem__)
-        env_guesses = [all_ids[sort_inds[i]] for i in range(5)]
-        err_msg = f"No registered env with id_: {id_}.\nDo you mean:\n"
-        for env_guess in env_guesses:
-            err_msg += f"    {env_guess}\n"
-        raise gym.error.UnregisteredEnv(err_msg) from e
+    # built in env_checker raises warnings or errors when ob not in observation_space
+    return gym.make(id_, disable_env_checker=True, **kwargs)
 
 
 def register(id_: str, **kwargs) -> None:
