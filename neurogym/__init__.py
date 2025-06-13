d39eb256a3320108fd23c203c79a62fcc39779a3
DaniBodor <d.bodor@esciencecenter.nl>
2025-06-14 01:07:00 +0200

    refactor: add __all__ to __init__.py

--- neurogym/__init__.py
+++ neurogym/__init__.py
@@ -2,3 +2,10 @@ from neurogym.core import BaseEnv, TrialEnv
 from neurogym.envs.registration import all_envs
 
 __version__ = "2.0.0"
+
+__all__ = [
+    "BaseEnv",
+    "TrialEnv",
+    "__version__",
+    "all_envs",
+]
