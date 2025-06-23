f32e2bc36bd9098d28d099ca9954cad801aa2c00
DaniBodor <d.bodor@esciencecenter.nl>
2025-06-23 15:13:15 +0200

    refactor: clean registration imports from init

--- neurogym/__init__.py
+++ neurogym/__init__.py
@@ -1,13 +1,5 @@
-# ruff: noqa: I001 - Import block is un-sorted or un-formatted
-# fixing rule above leads to problems in pytest
-# FIXME: figure out why this is the case and solve
-
+from neurogym.envs.registration import make, register
 from neurogym.utils import spaces
-from neurogym.envs.registration import make
-from neurogym.envs.registration import register
-from neurogym.envs.registration import all_envs
-from neurogym.envs.registration import all_tags
-from neurogym.envs.collections import get_collection
 from neurogym.utils.data import Dataset
 
 __version__ = "2.1.0"
