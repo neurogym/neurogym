afbbc527d878beac0952d8cb2b6be24c05e993af
DaniBodor <d.bodor@esciencecenter.nl>
2025-07-04 12:55:14 +0200

    refactor: avoid circular imports in init

--- neurogym/__init__.py
+++ neurogym/__init__.py
@@ -1,7 +1,3 @@
-from .envs.registration import make, register
-from .utils import info, spaces
-from .utils.data import Dataset
-
 try:
     import sb3_contrib
     import stable_baselines3
@@ -9,5 +5,10 @@ try:
     _SB3_INSTALLED = True
 except ImportError:
     _SB3_INSTALLED = False
+
+
+from .envs.registration import make, register
+from .utils import info, spaces
+from .utils.data import Dataset
 
 __version__ = "2.1.0"
